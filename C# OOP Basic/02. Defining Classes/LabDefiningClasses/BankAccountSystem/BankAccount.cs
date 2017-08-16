0d6ae3402d223d8dc0abd3ff0c86aebe0e4ac5f4
Simeon Valev <valev.simeon@outlook.com>
2017-08-16 15:20:05 +0300

    Upload Defining Classes

---
+++ C# OOP Basic/02. Defining Classes/LabDefiningClasses/BankAccountSystem/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿public class BankAccount
+{
+    private int id;
+    private double balance;
+
+    public int ID
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public double Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+
+    public void Deposit(double amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+        this.Balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.id}, balance {this.balance:F2}";
+    }
+}
---
+++ C# OOP Basic/02. Defining Classes/LabDefiningClasses/BankAccountSystem/BankAccountSystem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class BankAccountSystem
+{
+    private static void Main()
+    {
+        string line = Console.ReadLine();
+
+        Dictionary<int, BankAccount> account = new Dictionary<int, BankAccount>();
+
+        while (line != "End")
+        {
+            string[] items = line.Split();
+            string command = items[0];
+
+            switch (command)
+            {
+                case "Create":
+                    Create(items, account);
+                    break;
+
+                case "Deposit":
+                    Deposit(items, account);
+                    break;
+
+                case "Withdraw":
+                    Withdraw(items, account);
+                    break;
+
+                case "Print":
+                    Print(items, account);
+                    break;
+            }
+
+            line = Console.ReadLine();
+        }
+    }
+
+    private static void Print(string[] items, Dictionary<int, BankAccount> account)
+    {
+        int id = int.Parse(items[1]);
+        if (account.ContainsKey(id))
+        {
+            Console.WriteLine(account[id].ToString());
+        }
+        else
+        {
+            Console.WriteLine("Account does not exist");
+        }
+    }
+
+    private static void Withdraw(string[] items, Dictionary<int, BankAccount> account)
+    {
+        int id = int.Parse(items[1]);
+        double amount = double.Parse(items[2]);
+
+        if (account.ContainsKey(id))
+        {
+            if (account[id].Balance < amount)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else
+            {
+                account[id].Withdraw(amount);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Account does not exist");
+        }
+    }
+
+    private static void Deposit(string[] items, Dictionary<int, BankAccount> account)
+    {
+        int id = int.Parse(items[1]);
+        double amount = double.Parse(items[2]);
+
+        if (account.ContainsKey(id))
+        {
+                account[id].Deposit(amount);
+        }
+        else
+        {
+            Console.WriteLine("Account does not exist");
+        }
+    }
+
+    private static void Create(string[] items, Dictionary<int, BankAccount> account)
+    {
+        int id = int.Parse(items[1]);
+        if (account.ContainsKey(id))
+        {
+            Console.WriteLine("Account already exists");
+        }
+        else
+        {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.ID = id;
+            account.Add(id, bankAccount);
+        }
+    }
+}
