b20ce82547e4d644f9c33db39d374d624cb0bf2f
Simeon Valev <valev.simeon@outlook.com>
2017-08-23 13:35:39 +0300

    Fix all Bugs -Working Unit Tests and Full Source Code

--- C# OOP Advanced/22. Last Army/MissionController.Tests/MissionControllerTest.cs
+++ C# OOP Advanced/22. Last Army/MissionController.Tests/MissionControllerTest.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 [TestFixture]
 public class MissionControllerTest
 {
-    [Test]
-    public void FailMissionsOnHoldTestMethod()
+    private MissionController missionController;
+
+    [SetUp]
+    public void TestInitialize()
     {
-        Queue<IMission> missionQueue = new Queue<IMission>();
+        this.missionController = new MissionController(new Army(), new WareHouse());
+    }
 
-        missionQueue.Enqueue(new Easy(12.5));
-        missionQueue.Enqueue(new Hard(56));
-        missionQueue.Enqueue(new Hard(5));
+    [Test]
+    public void MissionsCountTest()
+    {
+        this.missionController.Missions.Enqueue(new Easy(12.5));
+        this.missionController.Missions.Enqueue(new Hard(56));
+        this.missionController.Missions.Enqueue(new Hard(5));
 
-        Assert.AreEqual(3, missionQueue.Count);
+        Assert.AreEqual(3, this.missionController.Missions.Count);
+    }
+
+    [Test]
+    public void FailedMissionCounterTest()
+    {
+        this.missionController.Missions.Enqueue(new Easy(12.5));
+        this.missionController.Missions.Enqueue(new Hard(56));
+        this.missionController.Missions.Enqueue(new Hard(5));
+        this.missionController.Missions.Enqueue(new Hard(6));
+
+        this.missionController.PerformMission(new Easy(23));
+
+        Assert.AreEqual(1, this.missionController.FailedMissionCounter);
+    }
+
+    [Test]
+    public void PerformMissionTest()
+    {
+        IArmy army = new Army();
+        army.AddSoldier(new Corporal("Pepo", 12, 23, 2));
+        army.AddSoldier(new Corporal("Ricky", 29, 29, 20));
+        IWareHouse wareHouse = new WareHouse();
+        wareHouse.AddAmmunitions("AutomaticMachine", 12);
+        wareHouse.AddAmmunitions("Gun", 3);
+        wareHouse.AddAmmunitions("Helmet", 5);
+        wareHouse.AddAmmunitions("Knife", 12);
+        wareHouse.AddAmmunitions("MachineGun", 3);
+        wareHouse.AddAmmunitions("NightVision", 5);
+        wareHouse.AddAmmunitions("RPG", 56);
+        MissionController mc = new MissionController(army, wareHouse);
+
+        mc.Missions.Enqueue(new Easy(12.5));
+        mc.Missions.Enqueue(new Hard(56));
+        mc.Missions.Enqueue(new Hard(5));
+        mc.Missions.Enqueue(new Hard(6));
+
+        string res = mc.PerformMission(new Easy(23));
+        int n = res.Length;
+
+        Assert.AreEqual(229, n);
     }
 }
