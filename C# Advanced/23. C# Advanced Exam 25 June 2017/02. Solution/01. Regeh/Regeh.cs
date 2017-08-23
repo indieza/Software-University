fe92828eaae7c95766e12cfabe4400f2bb343597
Simeon Valev <valev.simeon@outlook.com>
2017-08-24 01:10:37 +0300

    C# Advanced Exam 25 June 2017

---
+++ C# Advanced/23. C# Advanced Exam 25 June 2017/02. Solution/01. Regeh/Regeh.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+internal class Regeh
+{
+    private const string Template = @"\[[^\s\[]+<(\d+)REGEH(\d+)>[^]\s]+]";
+
+    private static void Main()
+    {
+        string line = Console.ReadLine();
+        Regex pattern = new Regex(Template);
+        MatchCollection matchCollection = pattern.Matches(line);
+
+        string resultText = string.Empty;
+        List<int> numbers = new List<int>();
+
+        int save = 0;
+
+        foreach (Match match in matchCollection)
+        {
+            int start = int.Parse(match.Groups[1].Value) + save;
+            int end = int.Parse(match.Groups[2].Value) + start;
+            save = end;
+            numbers.Add(start);
+            numbers.Add(end);
+        }
+
+        foreach (var number in numbers)
+        {
+            if (number > line.Length)
+            {
+                resultText += line[number % line.Length + 1];
+            }
+            else
+            {
+                resultText += line[number % line.Length];
+            }
+        }
+
+        Console.WriteLine(resultText);
+    }
+}
---
+++ C# Advanced/23. C# Advanced Exam 25 June 2017/02. Solution/02. KnightGame/KnightGame.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+internal class KnightGame
+{
+    private static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var matrix = FillMatrix(n);
+
+        var bestRow = 0;
+        var bestCol = 0;
+        var hittedHorses = 0;
+        var counter = 0;
+
+        while (true)
+        {
+            var savedHorse = 0;
+
+            for (var row = 0; row < n; row++)
+            {
+                for (var col = 0; col < n; col++)
+                {
+                    hittedHorses = CheckHittedHorses(matrix, row, col, n, hittedHorses);
+
+                    if (hittedHorses > savedHorse)
+                    {
+                        savedHorse = hittedHorses;
+                        bestRow = row;
+                        bestCol = col;
+                    }
+
+                    hittedHorses = 0;
+                }
+            }
+
+            if (savedHorse <= 0)
+            {
+                break;
+            }
+
+            matrix[bestRow, bestCol] = '0';
+            bestRow = 0;
+            bestCol = 0;
+            counter++;
+        }
+
+        Console.WriteLine(counter);
+    }
+
+    private static int CheckHittedHorses(char[,] matrix, int row, int col, int n, int hittedHorses)
+    {
+        if (matrix[row, col] == 'K')
+        {
+            if (row - 2 >= 0 && col + 1 < n && matrix[row - 2, col + 1] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row - 1 >= 0 && col + 2 < n && matrix[row - 1, col + 2] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row + 1 < n && col + 2 < n && matrix[row + 1, col + 2] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row + 2 < n && col + 1 < n && matrix[row + 2, col + 1] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row + 2 < n && col - 1 >= 0 && matrix[row + 2, col - 1] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row + 1 < n && col - 2 >= 0 && matrix[row + 1, col - 2] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row - 1 >= 0 && col - 2 >= 0 && matrix[row - 1, col - 2] == 'K')
+            {
+                hittedHorses++;
+            }
+            if (row - 2 >= 0 && col - 1 >= 0 && matrix[row - 2, col - 1] == 'K')
+            {
+                hittedHorses++;
+            }
+        }
+        return hittedHorses;
+    }
+
+    private static char[,] FillMatrix(int n)
+    {
+        var matrix = new char[n, n];
+
+        for (var i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine().ToCharArray();
+
+            for (var j = 0; j < n; j++)
+            {
+                matrix[i, j] = line[j];
+            }
+        }
+        return matrix;
+    }
+}
