ea295a0b06c6a9da63869acd7e92a07109100792
Simeon Valev <valev.simeon@outlook.com>
2017-08-24 13:20:55 +0300

    Upload final vison of the projects - C# Advanced Exam 2017

---
+++ C# Advanced/23. C# Advanced Exam 25 June 2017/02. Solution/03. NumberWars/NumberWars.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class NumberWars
+{
+    public static readonly string Alphabet = "#abcdefghijklmnopqrstuvwxyz";
+
+    private static void Main()
+    {
+        var turns = 0;
+        bool draw = false;
+
+        Queue<Card> playerOneDeck = new Queue<Card>();
+        Queue<Card> playerTwoDeck = new Queue<Card>();
+
+        var playerOneInputLine = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var playerTwoInputLine = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var card in playerOneInputLine)
+        {
+            int digit = int.Parse(card.Substring(0, card.Length - 1));
+            string letter = card.Substring(card.Length - 1);
+            playerOneDeck.Enqueue(new Card(digit, letter));
+        }
+
+        foreach (var card in playerTwoInputLine)
+        {
+            int digit = int.Parse(card.Substring(0, card.Length - 1));
+            string letter = card.Substring(card.Length - 1);
+            playerTwoDeck.Enqueue(new Card(digit, letter));
+        }
+
+        bool outOfcards = false;
+
+        while (turns < 1000000 && playerOneDeck.Count > 0 && playerTwoDeck.Count > 0)
+        {
+            if (turns > 10000)
+            {
+                turns = 1000000;
+                break;
+            }
+
+            List<Card> playedCards = new List<Card>();
+
+            var playerOneCard = playerOneDeck.Dequeue();
+            var playerTwoCard = playerTwoDeck.Dequeue();
+
+            playedCards.Add(playerOneCard);
+            playedCards.Add(playerTwoCard);
+
+            if (playerOneCard.Digit > playerTwoCard.Digit)
+            {
+                foreach (var card in playedCards.OrderByDescending(x => x.Digit).ThenByDescending(c => c.Name))
+                {
+                    playerOneDeck.Enqueue(card);
+                }
+            }
+            else if (playerOneCard.Digit < playerTwoCard.Digit)
+            {
+                foreach (var card in playedCards.OrderByDescending(x => x.Digit).ThenByDescending(c => c.Name))
+                {
+                    playerTwoDeck.Enqueue(card);
+                }
+            }
+            else
+            {
+                var haveWinner = false;
+
+                if (playerOneDeck.Count - 3 < 0 || playerTwoDeck.Count - 3 < 0)
+                {
+                    haveWinner = true;
+                    outOfcards = true;
+
+                    if (playerOneDeck.Count == playerTwoDeck.Count)
+                    {
+                        draw = true;
+                    }
+                }
+
+                while (haveWinner == false)
+                {
+                    if (playerOneDeck.Count - 3 < 0 || playerTwoDeck.Count - 3 < 0)
+                    {
+                        outOfcards = true;
+
+                        if (playerOneDeck.Count == playerTwoDeck.Count)
+                        {
+                            draw = true;
+                        }
+
+                        break;
+                    }
+
+                    List<Card> p1DrawCards = new List<Card>();
+                    List<Card> p2DrawCards = new List<Card>();
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        p1DrawCards.Add(playerOneDeck.Dequeue());
+                        p2DrawCards.Add(playerTwoDeck.Dequeue());
+                    }
+
+                    var drawResult = GetDrawWinner(p1DrawCards, p2DrawCards);
+
+                    playedCards.AddRange(p1DrawCards);
+                    playedCards.AddRange(p2DrawCards);
+
+                    if (drawResult == 1)
+                    {
+                        haveWinner = true;
+
+                        foreach (var card in playedCards.OrderByDescending(x => x.Digit).ThenByDescending(c => c.Name))
+                        {
+                            playerOneDeck.Enqueue(card);
+                        }
+                    }
+                    else if (drawResult == 2)
+                    {
+                        haveWinner = true;
+                        foreach (var card in playedCards.OrderByDescending(x => x.Digit).ThenByDescending(c => c.Name))
+                        {
+                            playerTwoDeck.Enqueue(card);
+                        }
+                    }
+                }
+            }
+
+            turns++;
+
+            if (outOfcards)
+            {
+                break;
+            }
+        }
+
+        if (draw)
+        {
+            Console.WriteLine($"Draw after {turns} turns");
+        }
+        else
+        {
+            Console.WriteLine(playerOneDeck.Count > playerTwoDeck.Count
+                ? $"First player wins after {turns} turns"
+                : $"Second player wins after {turns} turns");
+        }
+    }
+
+    private static int GetDrawWinner(List<Card> playerOneDrawCards, List<Card> playerTwoDrawCards)
+    {
+        string playerOneChar = string.Join(string.Empty, playerOneDrawCards.Select(x => x.Name)).ToLower();
+        string playerTwoChar = string.Join(string.Empty, playerTwoDrawCards.Select(x => x.Name)).ToLower();
+
+        var playerOneSum = 0;
+        var playerTwoSum = 0;
+
+        for (int i = 0; i < playerOneChar.Length; i++)
+        {
+            var num = Alphabet.IndexOf(playerOneChar[i]);
+            playerOneSum += num;
+        }
+
+        for (int i = 0; i < playerOneChar.Length; i++)
+        {
+            var num = Alphabet.IndexOf(playerTwoChar[i]);
+            playerTwoSum += num;
+        }
+
+        if (playerOneSum > playerTwoSum)
+        {
+            return 1;
+        }
+
+        if (playerOneSum < playerTwoSum)
+        {
+            return 2;
+        }
+
+        return 0;
+    }
+}
+
+internal class Card
+{
+    public Card(int digit, string name)
+    {
+        this.Digit = digit;
+        this.Name = name;
+    }
+
+    public int Digit { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ C# Advanced/23. C# Advanced Exam 25 June 2017/02. Solution/04. Hospital/Hospital.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Hospital
+{
+    internal class Hospital
+    {
+        private static void Main()
+        {
+            string input = Console.ReadLine();
+            List<Patient> list = new List<Patient>();
+
+            while (input != "Output")
+            {
+                string[] parts = input.Split(' ');
+
+                Patient patient = new Patient
+                {
+                    Department = parts[0],
+                    Doctor = parts[1] + " " + parts[2],
+                    Name = parts[3]
+                };
+
+                int room = list.Count(s => s.Department == parts[0]) / 3 + 1;
+
+                patient.Room = room;
+                list.Add(patient);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] parts = input.Split(' ');
+
+                if (parts.Length == 1)
+                {
+                    foreach (Patient patient in list.Where(p => p.Department == parts[0]))
+                    {
+                        Console.WriteLine(patient.Name);
+                    }
+                }
+                else
+                {
+                    int count;
+
+                    if (int.TryParse(parts[1], out count))
+                    {
+                        foreach (Patient p in list.Where(p => p.Department == parts[0] && p.Room == count).OrderBy(s => s.Name))
+                        {
+                            Console.WriteLine(p.Name);
+                        }
+                    }
+                    else
+                    {
+                        foreach (Patient p in list.Where(p => p.Doctor == parts[0] + " " + parts[1]).OrderBy(s => s.Name))
+                        {
+                            Console.WriteLine(p.Name);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+
+    public class Patient
+    {
+        public string Department { get; set; }
+
+        public string Doctor { get; set; }
+
+        public string Name { get; set; }
+
+        public int Room { get; set; }
+    }
+}
