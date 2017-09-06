1a178c801274461c826c04c688a73a7b5e2b6993
Simeon Valev <valev.simeon@outlook.com>
2017-09-06 15:22:57 +0300

    Final Vision Without Problem 4

---
+++ C# Advanced/28. C# Advanced Exam Retake - 3 September 2017/02. Solution/02. CryptoMaster/CryptoMaster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class CryptoMaster
+{
+    private static void Main()
+    {
+        List<int> sequance = Console.ReadLine()
+            .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        int max = 0;
+
+        for (int i = 0; i < sequance.Count; i++)
+        {
+            for (int step = 1; step <= sequance.Count; step++)
+            {
+                int currentNumber = sequance[i];
+                int indexOfNextNumber = (i + step) % sequance.Count;
+                int nextNumber = sequance[indexOfNextNumber];
+                int currentMax = 1;
+
+                while (currentNumber < nextNumber)
+                {
+                    currentMax++;
+                    int currentNumberIndex = indexOfNextNumber;
+                    indexOfNextNumber = (currentNumberIndex + step) % sequance.Count;
+                    currentNumber = nextNumber;
+                    nextNumber = sequance[indexOfNextNumber];
+                }
+
+                if (max < currentMax)
+                {
+                    max = currentMax;
+                }
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
