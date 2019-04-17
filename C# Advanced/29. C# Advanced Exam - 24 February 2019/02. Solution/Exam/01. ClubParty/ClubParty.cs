53899d498852fe75ed83342e26d202bf1c5656a4
Simeon Valev <simeonvalev00@gmail.com>
2019-04-17 19:40:35 +0300

    Start Working On First Problem

--- C# Advanced/29. C# Advanced Exam - 24 February 2019/02. Solution/Exam/01. ClubParty/ClubParty.cs
+++ C# Advanced/29. C# Advanced Exam - 24 February 2019/02. Solution/Exam/01. ClubParty/ClubParty.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _01.ClubParty
 {
     public class ClubParty
     {
+        private static readonly Regex AlphaPattern = new Regex(@"[a-zA-Z]");
+        private static readonly Regex NumsPattern = new Regex(@"[0-9]");
+
         private static void Main()
         {
+            int hallsCapacity = int.Parse(Console.ReadLine());
+            string[] items = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            List<string> halls = new List<string>();
+            List<int> capacity = new List<int>();
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                string item = items[i];
+
+                if (AlphaPattern.IsMatch(item))
+                {
+                    halls.Add(item);
+                }
+                else if (NumsPattern.IsMatch(item))
+                {
+                    capacity.Add(int.Parse(item.ToString()));
+                }
+            }
+
+            int saveCapacity = 0;
+
+            while (halls.Count != 0 || capacity.Count != 0)
+            {
+                int currentCapacity = capacity[0];
+
+                if (currentCapacity > hallsCapacity)
+                {
+                    capacity.RemoveAt(0);
+                }
+                else
+                {
+                    saveCapacity += currentCapacity;
+                }
+            }
         }
     }
 }
