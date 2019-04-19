cc6f5197791cd2d22e5347640d21fa24a471f593
Simeon Valev <simeonvalev00@gmail.com>
2019-04-19 10:52:52 +0300

    Problem 2 - Fill Table

--- C# Advanced/30. C# Advanced Exam - 17 Feb 2019/02. Solution/Exam/02. ExcelFunctions/ExcelFunctions.cs
+++ C# Advanced/30. C# Advanced Exam - 17 Feb 2019/02. Solution/Exam/02. ExcelFunctions/ExcelFunctions.cs
@@ -8,8 +8,40 @@ namespace _02.ExcelFunctions
 {
     public class ExcelFunctions
     {
+        private static int n = 0;
+        private static string[] items;
+        private static string[,] table;
         private static void Main()
         {
+            n = int.Parse(Console.ReadLine());
+            items = Console.ReadLine().Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            table = new string[n, items.Length];
+
+            FillFirstRoll();
+            FillTable();
+
+            string[] commandItems = Console.ReadLine().Split();
+        }
+
+        private static void FillFirstRoll()
+        {
+            for (int col = 0; col < items.Length; col++)
+            {
+                table[0, col] = items[col];
+            }
+        }
+
+        private static void FillTable()
+        {
+            for (int row = 1; row < n; row++)
+            {
+                string[] info = Console.ReadLine().Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < items.Length; col++)
+                {
+                    table[row, col] = info[col];
+                }
+            }
         }
     }
 }
