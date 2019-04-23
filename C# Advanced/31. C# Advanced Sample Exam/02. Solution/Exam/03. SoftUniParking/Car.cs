0fa10681aebe3915911bcecdf24266cdd2c214b3
Simeon Valev <simeonvalev00@gmail.com>
2019-04-23 19:23:34 +0300

    Start working on Problem 3 - 78/100

---
+++ C# Advanced/31. C# Advanced Sample Exam/02. Solution/Exam/03. SoftUniParking/Car.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace _03.SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int hoursePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = hoursePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+
+        public string RegistrationNumber
+        {
+            get
+            {
+                return this.registrationNumber;
+            }
+            set
+            {
+                this.registrationNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"HorsePower: {this.HorsePower}");
+            sb.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/31. C# Advanced Sample Exam/02. Solution/Exam/03. SoftUniParking/Parking.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+        private int count;
+
+        public Parking(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Cars = new List<Car>();
+            this.Count = this.Cars.Count;
+        }
+
+        public List<Car> Cars
+        {
+            get
+            {
+                return this.cars;
+            }
+            set
+            {
+                this.cars = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            set
+            {
+                this.capacity = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.Cars.Count;
+            }
+            set
+            {
+                this.count = this.Cars.Count;
+            }
+        }
+
+        public string AddCar(Car newCar)
+        {
+            if (this.Cars.Any(c => c.RegistrationNumber == newCar.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (this.Count >= this.Capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                this.Cars.Add(new Car(newCar.Make, newCar.Model, newCar.HorsePower, newCar.RegistrationNumber));
+                return $"Successfully added new car {newCar.Make} {newCar.RegistrationNumber}";
+            }
+        }
+
+        public string GetCar(string registrationNumber)
+        {
+            return this.Cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber).ToString();
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            bool isRemoved = false;
+
+            foreach (var car in this.Cars)
+            {
+                if (car.RegistrationNumber == registrationNumber)
+                {
+                    isRemoved = true;
+                    this.Cars.Remove(this.Cars.First(c => c.RegistrationNumber == registrationNumber));
+                    break;
+                }
+            }
+
+            return isRemoved == true
+                ? $"Successfully removed {registrationNumber}"
+                : "Car with that registration number, doesn't exist!";
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var number in registrationNumbers)
+            {
+                Console.WriteLine(RemoveCar(number));
+            }
+        }
+    }
+}
