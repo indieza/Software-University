43c31390bff4fe4e351a31ece33eb1ef8697d8f3
Simeon Valev <simeonvalev00@gmail.com>
2019-04-28 12:36:21 +0300

    Problem 1 - Done - 100/100

--- C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/01. SportCards/SportCards.cs
+++ C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/01. SportCards/SportCards.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.SportCards
 {
@@ -10,6 +8,52 @@ namespace _01.SportCards
     {
         private static void Main()
         {
+            Dictionary<string, Dictionary<string, decimal>> cards =
+                new Dictionary<string, Dictionary<string, decimal>>();
+
+            string line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                string[] items = line.Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (items[0] == "check")
+                {
+                    if (cards.ContainsKey(items[1]))
+                    {
+                        Console.WriteLine($"{items[1]} is available!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{items[1]} is not available!");
+                    }
+                }
+                else
+                {
+                    if (!cards.ContainsKey(items[0]))
+                    {
+                        cards.Add(items[0], new Dictionary<string, decimal>());
+                    }
+                    else if (!cards[items[0]].ContainsKey(items[1]))
+                    {
+                        cards[items[0]].Add(items[1], decimal.Parse(items[2]));
+                    }
+
+                    cards[items[0]][items[1]] = decimal.Parse(items[2]);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var card in cards.OrderByDescending(c => c.Value.Count))
+            {
+                Console.WriteLine($"{card.Key}:");
+
+                foreach (var sport in card.Value.OrderBy(s => s.Key))
+                {
+                    Console.WriteLine($"  -{sport.Key} - {Math.Round(sport.Value, 2)}");
+                }
+            }
         }
     }
 }
