92fe52f785db20321f157c68c8bd2d1d075b3458
Simeon Valev <simeonvalev00@gmail.com>
2019-04-28 13:00:00 +0300

    Problem 2 - Done - 100/100

--- C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/02. RobotCommunication/RobotCommunication.cs
+++ C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/02. RobotCommunication/RobotCommunication.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _02.RobotCommunication
 {
     public class RobotCommunication
     {
+        private static string pattern = @"([_,])([a-zA-Z]+)([\d])";
+
         private static void Main()
         {
+            string line = Console.ReadLine();
+
+            while (line != "Report")
+            {
+                MatchCollection collection = Regex.Matches(line, pattern);
+                List<string> text = new List<string>();
+
+                foreach (Match item in collection)
+                {
+                    char specialChar = char.Parse(item.Groups[1].Value);
+                    string letters = item.Groups[2].Value;
+                    int number = int.Parse(item.Groups[3].Value);
+
+                    text.Add(DecodeCode(specialChar, letters, number));
+                }
+
+                Console.WriteLine(string.Join(" ", text));
+
+                line = Console.ReadLine();
+            }
+        }
+
+        private static string DecodeCode(char specialChar, string letters, int number)
+        {
+            string result = string.Empty;
+
+            if (specialChar == ',')
+            {
+                foreach (char item in letters)
+                {
+                    result += (char)(number + (int)item);
+                }
+            }
+            else if (specialChar == '_')
+            {
+                foreach (char item in letters)
+                {
+                    result += (char)((int)item - number);
+                }
+            }
+
+            return result;
         }
     }
 }
