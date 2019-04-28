e3388b5f510ff79134e0f5670393864e73aecebc
Simeon Valev <simeonvalev00@gmail.com>
2019-04-28 11:13:48 +0300

    Upload Problems 1, 2, 3 and 4

---
+++ C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/03. Bombs/Bombs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Bombs
+{
+    public class Bombs
+    {
+        private static void Main()
+        {
+        }
+    }
+}
