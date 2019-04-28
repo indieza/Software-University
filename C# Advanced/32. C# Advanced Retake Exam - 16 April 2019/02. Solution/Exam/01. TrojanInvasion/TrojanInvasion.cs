c8c3e3316ee51f565e6621a205124c206829537a
Simeon Valev <simeonvalev00@gmail.com>
2019-04-28 11:00:17 +0300

    Problem 1 - Done!!! - 100/100

--- C# Advanced/32. C# Advanced Retake Exam - 16 April 2019/02. Solution/Exam/01. TrojanInvasion/TrojanInvasion.cs
+++ C# Advanced/32. C# Advanced Retake Exam - 16 April 2019/02. Solution/Exam/01. TrojanInvasion/TrojanInvasion.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.TrojanInvasion
 {
@@ -12,6 +10,57 @@ namespace _01.TrojanInvasion
         {
             int waves = int.Parse(Console.ReadLine());
             List<int> platesSpartanDefense = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> warriors = new List<int>();
+
+            for (int wave = 1; wave <= waves; wave++)
+            {
+                if (platesSpartanDefense.Count == 0)
+                {
+                    break;
+                }
+                warriors = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+                if (wave % 3 == 0)
+                {
+                    platesSpartanDefense.Add(int.Parse(Console.ReadLine()));
+                }
+
+                while (warriors.Count != 0 && platesSpartanDefense.Count != 0)
+                {
+                    int currentWarrier = warriors.Last();
+                    int currentPlate = platesSpartanDefense[0];
+
+                    if (currentWarrier == currentPlate)
+                    {
+                        warriors.RemoveAt(warriors.Count - 1);
+                        platesSpartanDefense.RemoveAt(0);
+                    }
+                    else if (currentPlate > currentWarrier)
+                    {
+                        currentPlate -= currentWarrier;
+                        warriors.RemoveAt(warriors.Count - 1);
+                        platesSpartanDefense[0] = currentPlate;
+                    }
+                    else if (currentWarrier > currentPlate)
+                    {
+                        currentWarrier -= currentPlate;
+                        platesSpartanDefense.RemoveAt(0);
+                        warriors.RemoveAt(warriors.Count - 1);
+                        warriors.Add(currentWarrier);
+                    }
+                }
+            }
+
+            if (platesSpartanDefense.Count > 0)
+            {
+                Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", platesSpartanDefense)}");
+            }
+            else
+            {
+                Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {string.Join(", ", warriors.ToArray().Reverse())}");
+            }
         }
     }
 }
