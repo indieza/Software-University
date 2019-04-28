c643f0117bda47c02ee3a5f633e4e26342a5ca39
Simeon Valev <simeonvalev00@gmail.com>
2019-04-28 14:50:13 +0300

    Problem 4 - Done - 100/100

--- C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/04. TheKitchen/TheKitchen.cs
+++ C# Advanced/33. C# Advanced Exam Retake - 17 December 2018/02. Solution/Exam/04. TheKitchen/TheKitchen.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _04.TheKitchen
 {
@@ -10,6 +8,37 @@ namespace _04.TheKitchen
     {
         private static void Main()
         {
+            List<int> knives = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> forks = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> sets = new List<int>();
+
+            while (knives.Count != 0 && forks.Count != 0)
+            {
+                int knife = knives.Last();
+                int fork = forks[0];
+
+                if (knife > fork)
+                {
+                    sets.Add(knife + fork);
+                    knives.RemoveAt(knives.Count - 1);
+                    forks.RemoveAt(0);
+                }
+                else if (fork > knife)
+                {
+                    knives.RemoveAt(knives.Count - 1);
+                }
+                else if (fork == knife)
+                {
+                    forks.RemoveAt(0);
+                    knife++;
+                    knives.RemoveAt(knives.Count - 1);
+                    knives.Add(knife);
+                }
+            }
+
+            Console.WriteLine($"The biggest set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
         }
     }
 }
