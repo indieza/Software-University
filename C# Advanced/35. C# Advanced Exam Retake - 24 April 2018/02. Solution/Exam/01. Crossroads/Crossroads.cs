2b50414af7d0fc3fc0d479d724029c40c03662d0
Simeon Valev <simeonvalev00@gmail.com>
2019-04-29 10:10:47 +0300

    Upload Problems

---
+++ C# Advanced/35. C# Advanced Exam Retake - 24 April 2018/02. Solution/Exam/01. Crossroads/Crossroads.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Crossroads
+{
+    public class Crossroads
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ C# Advanced/35. C# Advanced Exam Retake - 24 April 2018/02. Solution/Exam/02. ParkingFeud/ParkingFeud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.ParkingFeud
+{
+    public class ParkingFeud
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
