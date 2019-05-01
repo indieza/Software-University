c2bd11280719523fc82b4647301131cfc6fc33dc
Simeon Valev <simeonvalev00@gmail.com>
2019-05-01 08:55:18 +0300

    Problem 1 - Done 100/100

--- C# Advanced/35. C# Advanced Exam Retake - 24 April 2018/02. Solution/Exam/01. Crossroads/Crossroads.cs
+++ C# Advanced/35. C# Advanced Exam Retake - 24 April 2018/02. Solution/Exam/01. Crossroads/Crossroads.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.Crossroads
 {
@@ -10,6 +8,52 @@ namespace _01.Crossroads
     {
         private static void Main()
         {
+            int greenLightLength = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+
+            Queue<string> lane = new Queue<string>();
+            int totalCarsPassed = 0;
+
+            string line;
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                if (line != "green")
+                {
+                    lane.Enqueue(line);
+                    continue;
+                }
+
+                int currentGreenLight = greenLightLength;
+
+                string passingCar = null;
+                while (currentGreenLight > 0 && lane.Any())
+                {
+                    passingCar = lane.Dequeue();
+                    currentGreenLight -= passingCar.Length;
+                    totalCarsPassed++;
+                }
+
+                int freeWindowLeft = freeWindow + currentGreenLight;
+
+                if (freeWindowLeft < 0)
+                {
+                    int symbolsThatDidntPass = Math.Abs(freeWindowLeft);
+                    int indexOfHitSymbol = passingCar.Length - symbolsThatDidntPass;
+                    char symbolHit = passingCar[indexOfHitSymbol];
+                    Crash(passingCar, symbolHit);
+                }
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+        }
+
+        private static void Crash(string car, char symbolHit)
+        {
+            Console.WriteLine("A crash happened!");
+            Console.WriteLine($"{car} was hit at {symbolHit}.");
+            Environment.Exit(0);
         }
     }
 }
