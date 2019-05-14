664d74789e82edf1f4afdd04a4f77d22038c8ed2
Simeon Valev <simeonvalev00@gmail.com>
2019-05-14 11:50:29 +0300

    All Problems - Done!!! - 700/700 - Remove bin and obj

---
+++ C# Advanced - май 2019/01. Homeworks/01. Stacks and Queues - Lab/Lab/08. TrafficJam/TrafficJam.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    public class TrafficJam
+    {
+        private static void Main()
+        {
+            Queue<string> cars = new Queue<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            int passedCars = 0;
+
+            while (line != "end")
+            {
+                if (line == "green")
+                {
+                    int carsCount = Math.Min(n, cars.Count);
+
+                    for (int i = 0; i < carsCount; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        passedCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(line);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
