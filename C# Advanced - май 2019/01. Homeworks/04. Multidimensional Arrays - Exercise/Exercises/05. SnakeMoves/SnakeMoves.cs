b65df9ac6a9b89c00d676c6d16eda957269b9e59
Simeon Valev <simeonvalev00@gmail.com>
2019-05-16 19:54:16 +0300

    Problem 5 - Done!!! - 100/100

---
+++ C# Advanced - май 2019/01. Homeworks/04. Multidimensional Arrays - Exercise/Exercises/05. SnakeMoves/SnakeMoves.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    public class SnakeMoves
+    {
+        private static void Main()
+        {
+            int[] rowsCols = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+
+            char[] word = Console.ReadLine().ToCharArray();
+            int count = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (count == word.Length)
+                    {
+                        count = 0;
+                    }
+
+                    Console.Write($"{word[count]}");
+                    count++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
