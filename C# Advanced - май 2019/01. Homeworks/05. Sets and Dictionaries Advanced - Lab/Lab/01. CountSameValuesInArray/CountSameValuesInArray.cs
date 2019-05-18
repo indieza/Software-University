0e6829b655e2b105fa99da63faa2d2cfc6049ba4
Simeon Valev <simeonvalev00@gmail.com>
2019-05-18 10:57:43 +0300

    Problem 1 And 2 - Done!!! - 200/200

---
+++ C# Advanced - май 2019/01. Homeworks/05. Sets and Dictionaries Advanced - Lab/Lab/01. CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    public class CountSameValuesInArray
+    {
+        private static void Main()
+        {
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            double[] items = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (var item in items)
+            {
+                if (!numbers.ContainsKey(item))
+                {
+                    numbers.Add(item, 1);
+                }
+                else
+                {
+                    numbers[item]++;
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
---
+++ C# Advanced - май 2019/01. Homeworks/05. Sets and Dictionaries Advanced - Lab/Lab/02. AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    public class AverageStudentGrades
+    {
+        private static void Main()
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] items = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = items[0];
+                double mark = double.Parse(items[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+
+                students[name].Add(mark);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var mark in student.Value)
+                {
+                    Console.Write($"{mark:F2} ");
+                }
+
+                Console.WriteLine($"(avg: {student.Value.Sum() / student.Value.Count():F2})");
+            }
+        }
+    }
+}
