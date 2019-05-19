e3a9b3750329402ae68577b061aaf58f2064504e
Simeon Valev <simeonvalev00@gmail.com>
2019-05-19 18:16:24 +0300

    Fill Contest Information

---
+++ C# Advanced - май 2019/01. Homeworks/06. Sets and Dictionaries Advanced - Exercise/Exercise/08. Ranking/Ranking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Ranking
+{
+    public class Ranking
+    {
+        private static void Main()
+        {
+            string line = Console.ReadLine();
+
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+
+            while (line!= "end of contests")
+            {
+                string[] items = line.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string contest = items[0];
+                string password = items[1];
+
+                contests.Add(contest, password);
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
