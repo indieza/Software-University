aa8fcddc54fec80a41f9e10a316009b87fa34a4c
Simeon Valev <simeonvalev00@gmail.com>
2019-05-19 18:56:41 +0300

    All Problems - Done!!! - 800/800

--- C# Advanced - май 2019/01. Homeworks/06. Sets and Dictionaries Advanced - Exercise/Exercise/08. Ranking/Ranking.cs
+++ C# Advanced - май 2019/01. Homeworks/06. Sets and Dictionaries Advanced - Exercise/Exercise/08. Ranking/Ranking.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08.Ranking
 {
@@ -14,7 +12,7 @@ namespace _08.Ranking
 
             Dictionary<string, string> contests = new Dictionary<string, string>();
 
-            while (line!= "end of contests")
+            while (line != "end of contests")
             {
                 string[] items = line.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -25,6 +23,83 @@ namespace _08.Ranking
 
                 line = Console.ReadLine();
             }
+
+            line = Console.ReadLine();
+
+            Dictionary<string, Dictionary<string, int>> competitors = new Dictionary<string, Dictionary<string, int>>();
+
+            while (line != "end of submissions")
+            {
+                string[] items = line.Split(new[] { "=>" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string contest = items[0];
+                string password = items[1];
+                string username = items[2];
+                int points = int.Parse(items[3]);
+
+                if (contests.ContainsKey(contest))
+                {
+                    if (contests[contest] == password)
+                    {
+                        if (!competitors.ContainsKey(username))
+                        {
+                            competitors.Add(username, new Dictionary<string, int>());
+                        }
+                        if (competitors.ContainsKey(username))
+                        {
+                            if (competitors[username].ContainsKey(contest))
+                            {
+                                if (competitors[username][contest] < points)
+                                {
+                                    competitors[username][contest] = points;
+                                }
+                            }
+                            else
+                            {
+                                competitors[username].Add(contest, points);
+                            }
+                        }
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            string bestCompetitor = FindTheBest(competitors);
+
+            Console.WriteLine(
+                $"Best candidate is {bestCompetitor} with total {competitors[bestCompetitor].Sum(p => p.Value)} points.");
+
+            Console.WriteLine("Ranking:");
+
+            foreach (var competitor in competitors.OrderBy(c => c.Key))
+            {
+                Console.WriteLine($"{competitor.Key}");
+
+                foreach (var competition in competitor.Value.OrderByDescending(c => c.Value))
+                {
+                    Console.WriteLine($"#  {competition.Key} -> {competition.Value}");
+                }
+            }
+        }
+
+        private static string FindTheBest(Dictionary<string, Dictionary<string, int>> competitors)
+        {
+            int maxPoints = -1;
+            string name = string.Empty;
+
+            foreach (var competitor in competitors)
+            {
+                int points = competitor.Value.Sum(c => c.Value);
+
+                if (points > maxPoints)
+                {
+                    maxPoints = points;
+                    name = competitor.Key;
+                }
+            }
+
+            return name;
         }
     }
 }
