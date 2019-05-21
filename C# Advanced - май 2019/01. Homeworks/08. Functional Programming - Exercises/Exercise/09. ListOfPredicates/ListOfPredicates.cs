86167351a97b2bba90435a5b378f29c2eeb2db33
Simeon Valev <simeonvalev00@gmail.com>
2019-05-21 18:14:32 +0300

    Problem 9 and 10 - Done!!! - 200/200

---
+++ C# Advanced - май 2019/01. Homeworks/08. Functional Programming - Exercises/Exercise/09. ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    public class ListOfPredicates
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> deviders = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> nums = new List<int>();
+
+            bool Check(int x, int y) => x % y != 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isStillSearch = true;
+
+                foreach (var devider in deviders)
+                {
+                    if (Check(i, devider))
+                    {
+                        isStillSearch = false;
+                        break;
+                    }
+                }
+
+                if (isStillSearch)
+                {
+                    nums.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
---
+++ C# Advanced - май 2019/01. Homeworks/08. Functional Programming - Exercises/Exercise/10. PredicateParty/PredicateParty.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.PredicateParty
+{
+    public class PredicateParty
+    {
+        private static void Main()
+        {
+            List<string> guests = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "Party!")
+            {
+                string[] commandItems = command
+                    .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string condition = commandItems[2];
+
+                Func<string, bool> filter;
+
+                switch (commandItems[0])
+                {
+                    case "Remove" when commandItems[1] == "StartsWith":
+                        filter = x => !x.StartsWith(condition);
+                        break;
+
+                    case "Remove" when commandItems[1] == "EndsWith":
+                        filter = x => !x.EndsWith(condition);
+                        break;
+
+                    case "Remove":
+                        filter = x => x.Length != int.Parse(condition);
+                        break;
+
+                    case "Double" when commandItems[1] == "StartsWith":
+                        filter = x => x.StartsWith(condition);
+                        break;
+
+                    case "Double" when commandItems[1] == "EndsWith":
+                        filter = x => x.EndsWith(condition);
+                        break;
+
+                    default:
+                        filter = x => x.Length == int.Parse(condition);
+                        break;
+                }
+
+                if (commandItems[0] == "Remove")
+                {
+                    guests = guests.Where(filter).ToList();
+                }
+                else
+                {
+                    List<string> tempList = guests.Where(filter).ToList();
+
+                    foreach (var name in tempList)
+                    {
+                        var index = guests.IndexOf(name);
+                        guests.Insert(index, name);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Any()
+                ? $"{string.Join(", ", guests)} are going to the party!"
+                : "Nobody is going to the party!");
+        }
+    }
+}
