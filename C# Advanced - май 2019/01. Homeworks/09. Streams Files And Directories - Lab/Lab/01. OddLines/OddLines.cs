77176794919bd9112e0d602f7033b5befad02bd1
Simeon Valev <simeonvalev00@gmail.com>
2019-06-01 15:34:18 +0300

    Problems 1 And 2 - Done!!! - 200/200

---
+++ C# Advanced - май 2019/01. Homeworks/09. Streams Files And Directories - Lab/Lab/01. OddLines/OddLines.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace _01.OddLines
+{
+    public class OddLines
+    {
+        private static void Main()
+        {
+            string filePath = Path.Combine("Files", "Input.txt");
+
+            StreamReader reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter("Output.txt");
+
+                using (writer)
+                {
+                    int count = 0;
+
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        if (count % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        count++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced - май 2019/01. Homeworks/09. Streams Files And Directories - Lab/Lab/02. LineNumbers/LineNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace _02.LineNumbers
+{
+    public class LineNumbers
+    {
+        private static void Main()
+        {
+            string filePath = Path.Combine("Files", "Input.txt");
+
+            StreamReader reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter("Output.txt");
+
+                using (writer)
+                {
+                    int counter = 1;
+
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
