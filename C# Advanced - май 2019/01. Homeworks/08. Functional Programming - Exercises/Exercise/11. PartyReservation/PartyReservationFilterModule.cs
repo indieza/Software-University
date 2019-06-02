69603af5163241233cb6472e6fcf31aaf6078661
Simeon Valev <simeonvalev00@gmail.com>
2019-06-02 18:26:06 +0300

    Problems 11 And 13 - Done!!! - 200/200

---
+++ C# Advanced - май 2019/01. Homeworks/08. Functional Programming - Exercises/Exercise/11. PartyReservation/PartyReservationFilterModule.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PartyReservation
+{
+    public class PartyReservationFilterModule
+    {
+        private static void Main()
+        {
+            List<string> people = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, List<string>> filters = new Dictionary<string, List<string>>();
+
+            string line = Console.ReadLine();
+
+            while (line != "Print")
+            {
+                string[] commandItems = line.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string command = commandItems[0];
+                string filterType = commandItems[1];
+                string parameter = commandItems[2];
+
+                if (command == "Add filter")
+                {
+                    if (!filters.ContainsKey(filterType))
+                    {
+                        filters.Add(filterType, new List<string>());
+                    }
+                    if (!filters[filterType].Contains(parameter))
+                    {
+                        filters[filterType].Add(parameter);
+                    }
+                }
+                else if (command == "Remove filter")
+                {
+                    if (filters.ContainsKey(filterType))
+                    {
+                        filters[filterType].Remove(parameter);
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                if (filter.Key == "Length")
+                {
+                    foreach (var condition in filter.Value)
+                    {
+                        people = people.Where(p => p.Length != int.Parse(condition)).ToList();
+                    }
+                }
+                else if (filter.Key == "Starts with")
+                {
+                    foreach (var condition in filter.Value)
+                    {
+                        people = people.Where(p => !p.StartsWith(condition)).ToList();
+                    }
+                }
+                else if (filter.Key == "Ends with")
+                {
+                    foreach (var condition in filter.Value)
+                    {
+                        people = people.Where(p => !p.EndsWith(condition)).ToList();
+                    }
+                }
+                else if (filter.Key == "Contains")
+                {
+                    foreach (var condition in filter.Value)
+                    {
+                        people = people.Where(p => !p.Contains(condition)).ToList();
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", people));
+        }
+    }
+}
---
+++ C# Advanced - май 2019/01. Homeworks/08. Functional Programming - Exercises/Exercise/13. TriFunction/TriFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13._TriFunction
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                 .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            string result = names.FirstOrDefault(n => n.ToCharArray().Select(c => (int)c).Sum() >= length);
+
+            Console.WriteLine(result);
+        }
+    }
+}
