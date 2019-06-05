bfc9d36bbc8fe66f09ca9e0e08892460fa6be143
Simeon Valev <simeonvalev00@gmail.com>
2019-06-05 20:21:16 +0300

    Write Up-Command

--- C# Advanced - май 2019/02. Exams/01. C# Advanced Retake Exam - 16 April 2019/02. Solution/Exam/02. HelensAbduction/HelensAbduction.cs
+++ C# Advanced - май 2019/02. Exams/01. C# Advanced Retake Exam - 16 April 2019/02. Solution/Exam/02. HelensAbduction/HelensAbduction.cs
@@ -10,42 +10,142 @@ namespace _02.HelensAbduction
     {
         private static int parisEnergy;
         private static int rows;
+        private static int cols;
+        private static char[] currentRow;
         private static char[,] field;
         private static string[] commandItems;
         private static string command;
         private static int targetRow;
         private static int targetCol;
+        private static int parisRow;
+        private static int parisCol;
+        private static bool isDie;
 
         private static void Main()
         {
             parisEnergy = int.Parse(Console.ReadLine());
             rows = int.Parse(Console.ReadLine());
+            currentRow = Console.ReadLine().ToCharArray();
+            cols = currentRow.Count();
 
-            field = new char[rows, rows];
+            field = new char[rows, cols];
+            isDie = false;
 
             FillFiled();
 
-            while (true)
+            while (!isDie)
             {
                 ExecuteCommandParameters();
+                field[targetRow, targetCol] = 'S';
 
-                switch (command.ToLower())
-                {
-                    case "up":
-                        break;
+                ProcessingCommand();
+            }
 
-                    case "down":
-                        break;
+            PrintField();
+        }
 
-                    case "left":
-                        break;
+        private static void ProcessingCommand()
+        {
+            switch (command.ToLower())
+            {
+                case "up":
+                    if (parisRow - 1 < 0)
+                    {
+                        parisEnergy--;
 
-                    case "right":
-                        break;
+                        if (parisEnergy <= 0)
+                        {
+                            isDie = false;
+                            field[parisRow, parisCol] = 'X';
+                            Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+                        }
+                    }
+                    else
+                    {
+                        field[parisRow, parisCol] = '-';
+                        parisRow--;
+                        parisEnergy--;
 
-                    default:
-                        break;
-                }
+                        if (parisEnergy <= 0)
+                        {
+                            isDie = true;
+                            field[parisRow, parisCol] = 'X';
+                            Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+                        }
+                        else
+                        {
+                            if (field[parisRow, parisCol] == 'S')
+                            {
+                                parisEnergy -= 2;
+
+                                if (parisEnergy <= 0)
+                                {
+                                    isDie = true;
+                                    field[parisRow, parisCol] = 'X';
+                                    Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+                                }
+                                else
+                                {
+                                    field[parisRow, parisCol] = '-';
+                                }
+                            }
+                            else if (field[parisRow, parisCol] == 'H')
+                            {
+                                isDie = true;
+                                field[parisRow, parisCol] = '-';
+                                Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {parisEnergy}");
+                            }
+                        }
+                    }
+                    break;
+
+                case "down":
+                    if (parisRow + 1 >= rows)
+                    {
+                        parisEnergy--;
+                        //Check Alive
+                    }
+                    else
+                    {
+                        field[parisRow, parisCol] = '-';
+                        parisRow++;
+                        parisEnergy--;
+                        //Check Alive
+                    }
+                    break;
+
+                case "left":
+                    if (parisCol - 1 < 0)
+                    {
+                        parisEnergy--;
+                        //CheckAlive
+                    }
+                    else
+                    {
+                        field[parisRow, parisCol] = '-';
+                        parisCol--;
+                        parisEnergy--;
+                        //Check Alive
+                    }
+                    break;
+
+                case "right":
+                    if (parisCol + 1 >= cols)
+                    {
+                        parisEnergy--;
+                        //Check Alive
+                    }
+                    else
+                    {
+                        field[parisRow, parisCol] = '-';
+                        parisCol++;
+                        parisEnergy--;
+                        //Check Alive
+                    }
+                    break;
+
+                default:
+                    break;
             }
         }
 
@@ -61,14 +161,45 @@ namespace _02.HelensAbduction
 
         private static void FillFiled()
         {
-            for (int row = 0; row < rows; row++)
+            for (int col = 0; col < rows; col++)
             {
-                char[] currentRow = Console.ReadLine().ToCharArray();
+                if (currentRow[col] == 'P')
+                {
+                    parisRow = 0;
+                    parisCol = col;
+                }
+
+                field[0, col] = currentRow[col];
+            }
+            for (int row = 1; row < rows; row++)
+            {
+                currentRow = Console.ReadLine().ToCharArray();
 
                 for (int col = 0; col < rows; col++)
                 {
+                    if (currentRow[col] == 'P')
+                    {
+                        parisRow = row;
+                        parisCol = col;
+                    }
+
                     field[row, col] = currentRow[col];
                 }
+            }
+        }
+
+        private static void PrintField()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                List<char> currentRow = new List<char>();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    currentRow.Add(field[row, col]);
+                }
+
+                Console.WriteLine(string.Join("", currentRow));
             }
         }
     }
