32c1f7f7273d50fa0dfea4b9abf12234ccfdd0aa
Simeon Valev <simeonvalev00@gmail.com>
2019-06-10 17:35:02 +0300

    Problem 1 - Done!!! - 100/100

--- C# Advanced - май 2019/02. Exams/03. (Demo) C# Advanced Exam - 17 Feb 2019/02. Solution/Exam/01. Socks/Socks.cs
+++ C# Advanced - май 2019/02. Exams/03. (Demo) C# Advanced Exam - 17 Feb 2019/02. Solution/Exam/01. Socks/Socks.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.Socks
 {
@@ -10,6 +8,36 @@ namespace _01.Socks
     {
         private static void Main()
         {
+            Stack<int> leftSocks = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> rightSocks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            List<int> pairs = new List<int>();
+
+            while (leftSocks.Count() != 0 && rightSocks.Count() != 0)
+            {
+                int currentLeftSock = leftSocks.Peek();
+                int currentRightSock = rightSocks.Peek();
+
+                if (currentLeftSock > currentRightSock)
+                {
+                    pairs.Add(currentLeftSock + currentRightSock);
+                    leftSocks.Pop();
+                    rightSocks.Dequeue();
+                }
+                else if (currentRightSock > currentLeftSock)
+                {
+                    leftSocks.Pop();
+                }
+                else if (currentLeftSock == currentRightSock)
+                {
+                    rightSocks.Dequeue();
+                    leftSocks.Pop();
+                    leftSocks.Push(currentLeftSock + 1);
+                }
+            }
+
+            Console.WriteLine($"{pairs.Max()}");
+            Console.WriteLine(string.Join(" ", pairs));
         }
     }
 }
