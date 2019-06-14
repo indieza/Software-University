9bc5a526546569e78ce3fe5b274dcc4276affa2c
Simeon Valev <simeonvalev00@gmail.com>
2019-06-14 11:02:53 +0300

    Problem 1 - Done!!! - 100/100

--- C# Advanced - май 2019/02. Exams/04. C# Advanced Exam - 24 February 2019/02. Solution/Exam/01. ClubParty/ClubParty.cs
+++ C# Advanced - май 2019/02. Exams/04. C# Advanced Exam - 24 February 2019/02. Solution/Exam/01. ClubParty/ClubParty.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.ClubParty
 {
@@ -10,6 +8,43 @@ namespace _01.ClubParty
     {
         private static void Main()
         {
+            int hallsMaxCapacity = int.Parse(Console.ReadLine());
+            Stack<string> symbols = new Stack<string>(Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            List<string> halls = new List<string>();
+            List<int> people = new List<int>();
+
+            while (symbols.Count != 0)
+            {
+                string currentSymbols = symbols.Pop();
+
+                if (int.TryParse(currentSymbols, out int capacityNeeded))
+                {
+                    if (halls.Count != 0)
+                    {
+                        if (people.Sum() + capacityNeeded <= hallsMaxCapacity)
+                        {
+                            people.Add(capacityNeeded);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{halls[0]} -> {string.Join(", ", people)}");
+                            halls.RemoveAt(0);
+                            people.Clear();
+
+                            if (halls.Count != 0)
+                            {
+                                people.Add(capacityNeeded);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    halls.Add(currentSymbols);
+                }
+            }
         }
     }
 }
