d3c0e15fec9b32899d214133485b06691c83dd39
Simeon Valev <simeonvalev00@gmail.com>
2019-06-16 10:28:50 +0300

    Upload Conditions And Solution Construction

---
+++ C# Advanced - май 2019/02. Exams/05. (Demo) C# Advanced Exam - 16 June 2019/02. Solution/Exam/01. TheGarden/TheGarden.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.TheGarden
+{
+    public class TheGarden
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ C# Advanced - май 2019/02. Exams/05. (Demo) C# Advanced Exam - 16 June 2019/02. Solution/Exam/02. MakeSalad/MakeSalad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.MakeSalad
+{
+    public class MakeSalad
+    {
+        private static void Main()
+        {
+        }
+    }
+}
