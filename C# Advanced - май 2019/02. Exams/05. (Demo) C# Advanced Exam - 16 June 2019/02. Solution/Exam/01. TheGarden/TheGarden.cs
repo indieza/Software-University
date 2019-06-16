f8b03d6a2bb5172fc644b049c7fab229e114a852
Simeon Valev <simeonvalev00@gmail.com>
2019-06-16 19:03:58 +0300

    Problem 2 - Make First Command

--- C# Advanced - май 2019/02. Exams/05. (Demo) C# Advanced Exam - 16 June 2019/02. Solution/Exam/01. TheGarden/TheGarden.cs
+++ C# Advanced - май 2019/02. Exams/05. (Demo) C# Advanced Exam - 16 June 2019/02. Solution/Exam/01. TheGarden/TheGarden.cs
@@ -8,8 +8,93 @@ namespace _01.TheGarden
 {
     public class TheGarden
     {
+        private static int rows;
+        private static char[][] field;
+        private static string[] commandItems;
+        private static int targetRow;
+        private static int targetCol;
+        private static string directory;
+        private static Dictionary<char, int> information;
+
         private static void Main()
         {
+            rows = int.Parse(Console.ReadLine());
+            field = new char[rows][];
+
+            FillField();
+            information = new Dictionary<char, int>();
+
+            string line = Console.ReadLine();
+
+            while (line != "End of Harvest")
+            {
+                commandItems = line.Split();
+                ExecuteCommand();
+
+                line = Console.ReadLine();
+            }
+
+            PrintField();
+        }
+
+        private static void ExecuteCommand()
+        {
+            string command = commandItems[0];
+
+            switch (command)
+            {
+                case "Harvest":
+                    targetRow = int.Parse(commandItems[1]);
+                    targetCol = int.Parse(commandItems[2]);
+
+                    HarvestCell();
+                    break;
+
+                case "Mole":
+                    targetRow = int.Parse(commandItems[1]);
+                    targetCol = int.Parse(commandItems[2]);
+                    directory = commandItems[3];
+
+                    MoleCell();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static void MoleCell()
+        {
+        }
+
+        private static void HarvestCell()
+        {
+            if (targetRow >= 0 && targetRow <= rows - 1)
+            {
+                if (targetCol <= field[targetRow].Length - 1 && targetCol >= 0)
+                {
+                    if (field[targetRow][targetCol] != ' ')
+                    {
+                        field[targetRow][targetCol] = ' ';
+                    }
+                }
+            }
+        }
+
+        private static void FillField()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                field[row] = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            }
+        }
+
+        private static void PrintField()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(string.Join(" ", field[row]));
+            }
         }
     }
 }
