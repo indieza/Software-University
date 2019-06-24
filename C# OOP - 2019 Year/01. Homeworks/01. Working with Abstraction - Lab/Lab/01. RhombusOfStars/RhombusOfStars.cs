f9f8662a4e1d141a3f8b4b20571812ff6a512819
Simeon Valev <simeonvalev00@gmail.com>
2019-06-24 10:52:49 +0300

    Upload Conditions And Problem 1 - Done!!! - 100/100

---
+++ C# OOP - 2019 Year/01. Homeworks/01. Working with Abstraction - Lab/Lab/01. RhombusOfStars/RhombusOfStars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.RhombusOfStars
+{
+    public class RhombusOfStars
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                PrintRow(row, n);
+            }
+            for (int row = n - 2; row >= 0; row--)
+            {
+                PrintRow(row, n);
+            }
+        }
+
+        private static void PrintRow(int row, int n)
+        {
+            for (int i = 0; i < n - row; i++)
+            {
+                Console.Write($" ");
+            }
+            for (int i = 0; i < row + 1; i++)
+            {
+                Console.Write($"* ");
+            }
+            for (int i = 1; i < row; i++)
+            {
+                Console.Write($" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
