544972bb7c9be6cb7aa5963f74e78a898826e1ab
Simeon Valev <simeonvalev00@gmail.com>
2019-07-06 15:10:25 +0300

    Add Construction .Net Core 3.0

---
+++ C# OOP - 2019 Year/01. Homeworks/06. Encapsulation - Exercises/Exercises/FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballTeamGenerator
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
