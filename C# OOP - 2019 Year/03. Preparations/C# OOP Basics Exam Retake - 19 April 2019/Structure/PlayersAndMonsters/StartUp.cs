6fcab3387ff8305734e29e2da277c3e68936dd91
Simeon Valev <simeonvalev00@gmail.com>
2019-07-12 23:28:38 +0300

    Upload Condition And Sceleton

---
+++ C# OOP - 2019 Year/03. Preparations/C# OOP Basics Exam Retake - 19 April 2019/Structure/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace PlayersAndMonsters
+{
+    using Core;
+    using Core.Contracts;
+    using Core.Factories;
+    using Core.Factories.Contracts;
+    using Repositories;
+    using Repositories.Contracts;
+    using IO;
+    using IO.Contracts;
+    using Models.BattleFields;
+    using Models.BattleFields.Contracts;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
