efae20beefc968d7682ef24569976078d8765e2d
Simeon Valev <simeonvalev00@gmail.com>
2019-07-13 10:36:38 +0300

    Upload Condition

---
+++ C# OOP - 2019 Year/03. Preparations/C# OOP Basics Exam - 18 March 2018/Structure/DungeonsAndCodeWizards/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonsAndCodeWizards
+{
+	public class StartUp
+	{
+		// DO NOT rename this file's namespace or class name.
+		// However, you ARE allowed to use your own namespaces (or no namespaces at all if you prefer) in other classes.
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
