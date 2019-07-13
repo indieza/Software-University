ffa6122d3f473f8381d5cb901e05eec520c4b634
Simeon Valev <simeonvalev00@gmail.com>
2019-07-13 12:00:05 +0300

    Upload Condition

---
+++ C# OOP - 2019 Year/03. Preparations/04. C# OOP Retake Exam - 18 April 2019/Structure/PlayersAndMonsters/Core/ManagerController.cs
@@ -0,0 +1,38 @@
+﻿namespace PlayersAndMonsters.Core
+{
+    using System;
+
+    using Contracts;
+
+    public class ManagerController : IManagerController
+    {
+        public ManagerController()
+        {
+        }
+
+        public string AddPlayer(string type, string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddCard(string type, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddPlayerCard(string username, string cardName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Fight(string attackUser, string enemyUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
