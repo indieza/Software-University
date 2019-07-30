0096af6fe27fd0202bf73eabcc61e46eb862d0b8
Simeon Valev <simeonvalev00@gmail.com>
2019-07-30 15:15:45 +0300

    Uplaod Tests For First Problem

--- C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/Database.Tests/DatabaseTests.cs
+++ C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/Database.Tests/DatabaseTests.cs
@@ -1,18 +1,59 @@
 using NUnit.Framework;
+using System;
+using static Database.Database;
 
 namespace Tests
 {
     public class DatabaseTests
     {
+        private Database database;
+
         [SetUp]
         public void Setup()
         {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            this.database = new Database.Database(nums);
         }
 
         [Test]
-        public void Test1()
+        public void Test_Constructure()
         {
-            Assert.Pass();
+            Assert.AreEqual(16, database.Count);
+        }
+
+        [Test]
+        public void Test_Add_Exception()
+        {
+            Assert.Throws<InvalidOperationException>(() => database.Add(17));
+        }
+
+        [Test]
+        public void Test_Add()
+        {
+            this.database = new Database.Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            this.database.Add(11);
+            Assert.AreEqual(11, this.database.Count);
+        }
+
+        [Test]
+        public void Test_Remove_Element_From_Empty_Collection()
+        {
+            this.database = new Database.Database();
+
+            Assert.Throws<InvalidOperationException>(() => this.database.Remove());
+        }
+
+        [Test]
+        public void Test_Remove()
+        {
+            this.database.Remove();
+            Assert.AreEqual(15, this.database.Count);
+        }
+
+        [Test]
+        public void Test_Fetch()
+        {
+            Assert.AreEqual(16, this.database.Fetch().Length);
         }
     }
 }
