6010630b055925da766a1fdd6324309e7fc780da
Simeon Valev <simeonvalev00@gmail.com>
2019-07-30 11:29:17 +0300

    Upload Final Vision

---
+++ C# OOP - 2019 Year/01. Homeworks/15. Unit Tests - Lab/Lab/Tests/AxeTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private Hero hero;
+        private Axe axe;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.hero = new Hero("Name");
+            this.axe = new Axe(10, 10);
+        }
+
+        [Test]
+        public void Test_Hero_Constructure()
+        {
+            Assert.AreEqual("Name", this.hero.Name);
+            Assert.AreEqual(0, this.hero.Experience);
+        }
+
+        [Test]
+        public void Test_Axe_Constructure()
+        {
+            Assert.AreEqual(10, this.axe.AttackPoints);
+            Assert.AreEqual(10, this.axe.DurabilityPoints);
+        }
+    }
+}
