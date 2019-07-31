f36026c9cea898328b06d3176e20ec3aa081215b
Simeon Valev <simeonvalev00@gmail.com>
2019-07-31 19:16:41 +0300

    Improve Tests - Warrior, BattleField And CarManager

--- C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/FightingArena.Tests/ArenaTests.cs
+++ C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,44 @@
-﻿using NUnit.Framework;
+﻿using FightingArena;
+using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        private Arena arena;
+
         [SetUp]
         public void Setup()
         {
+            this.arena = new Arena();
         }
 
         [Test]
-        public void Test1()
+        public void Test_Constructure()
         {
-            Assert.Pass();
+            Assert.AreEqual(0, this.arena.Warriors.Count);
+        }
+
+        [Test]
+        public void Test_Enroll_Exception()
+        {
+            Warrior warrior1 = new Warrior("Name1", 10, 30);
+            Warrior warrior2 = new Warrior("Name1", 10, 10);
+
+            this.arena.Enroll(warrior1);
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(warrior2));
+        }
+
+        [Test]
+        public void Test_Enroll()
+        {
+            Warrior warrior1 = new Warrior("Name1", 10, 30);
+            Warrior warrior2 = new Warrior("Name2", 10, 10);
+
+            this.arena.Enroll(warrior1);
+            this.arena.Enroll(warrior2);
+            Assert.AreEqual(2, this.arena.Count);
         }
     }
 }
--- C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/FightingArena.Tests/WarriorTests.cs
+++ C# OOP - 2019 Year/01. Homeworks/16. Unit Testing - Exercises/01. Database_Skeleton/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,85 @@
+using FightingArena;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private Warrior warrior;
+
         [SetUp]
         public void Setup()
         {
+            this.warrior = new Warrior("Name", 10, 10);
         }
 
         [Test]
-        public void Test1()
+        public void Test_Constructure()
         {
-            Assert.Pass();
+            Assert.AreEqual("Name", this.warrior.Name);
+            Assert.AreEqual(10, this.warrior.Damage);
+            Assert.AreEqual(10, this.warrior.HP);
+        }
+
+        [Test]
+        public void Test_Name()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(null, 10, 10));
+        }
+
+        [Test]
+        public void Test_Damage()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Name", 0, 10));
+        }
+
+        [Test]
+        public void Test_HP()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Name", 10, -10));
+        }
+
+        [Test]
+        public void Test_Attack_First_Exception()
+        {
+            Warrior warrior1 = new Warrior("Name1", 10, 30);
+            Warrior warrior2 = new Warrior("Name2", 10, 10);
+            Assert.Throws<InvalidOperationException>(() => warrior1.Attack(warrior2));
+        }
+
+        [Test]
+        public void Test_Attack_Second_Exception()
+        {
+            Warrior warrior1 = new Warrior("Name1", 10, 40);
+            Warrior warrior2 = new Warrior("Name2", 10, 10);
+            Assert.Throws<InvalidOperationException>(() => warrior1.Attack(warrior2));
+        }
+
+        [Test]
+        public void Test_Attack_Third_Exception()
+        {
+            Warrior warrior1 = new Warrior("Name1", 10, 40);
+            Warrior warrior2 = new Warrior("Name2", 50, 40);
+            Assert.Throws<InvalidOperationException>(() => warrior1.Attack(warrior2));
+        }
+
+        [Test]
+        public void Test_Attack_If_Statment()
+        {
+            Warrior warrior1 = new Warrior("Name1", 41, 35);
+            Warrior warrior2 = new Warrior("Name2", 34, 40);
+            warrior1.Attack(warrior2);
+            Assert.AreEqual(0, warrior2.HP);
+        }
+
+        [Test]
+        public void Test_Attack_Else_Statment()
+        {
+            Warrior warrior1 = new Warrior("Name1", 39, 35);
+            Warrior warrior2 = new Warrior("Name2", 34, 40);
+            warrior1.Attack(warrior2);
+            Assert.AreEqual(1, warrior2.HP);
         }
     }
 }
