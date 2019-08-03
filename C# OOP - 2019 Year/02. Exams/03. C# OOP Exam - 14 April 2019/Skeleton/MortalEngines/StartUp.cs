af4259dd401f2cfd83c490db49dc4cfabb2e4009
Simeon Valev <simeonvalev00@gmail.com>
2019-08-03 18:21:16 +0300

    Upload Condition Of The Project

--- C# OOP - 2019 Year/02. Exams/03. C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/StartUp.cs
+++ C# OOP - 2019 Year/02. Exams/03. C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/StartUp.cs
@@ -1,13 +1,9 @@
-﻿using MortalEngines.Core;
-
-namespace MortalEngines
+﻿namespace MortalEngines
 {
     public class StartUp
     {
         public static void Main()
         {
-            Engine engine = new Engine();
-            engine.Run();
         }
     }
 }
