486719704bba1b6e18b9abefab33d91c280470e5
Simeon Valev <simeonvalev00@gmail.com>
2019-08-04 18:30:44 +0300

    Upload Structure Condition

---
+++ C# OOP - 2019 Year/02. Exams/07. C# OOP Demo Exam - 04 August 2019/01. Structure_Skeleton/MXGP/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MXGP
+{
+    using Models.Motorcycles;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+         //TODO Add IEngine
+         Motorcycle varche = new PowerMotorcycle("12214235", 75);
+         Console.WriteLine(varche.HorsePower);
+        }
+    }
+}
