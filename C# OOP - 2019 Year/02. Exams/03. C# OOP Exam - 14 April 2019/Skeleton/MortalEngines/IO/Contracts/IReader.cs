0265c1d5051c9605424b7e164f8e8dc63139501c
Simeon Valev <simeonvalev00@gmail.com>
2019-08-06 17:34:36 +0300

    Upload Structure Condition

---
+++ C# OOP - 2019 Year/02. Exams/03. C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/IO/Contracts/IReader.cs
@@ -0,0 +1,10 @@
+﻿namespace MortalEngines.IO.Contracts
+{
+    using MortalEngines.Core.Contracts;
+    using System.Collections.Generic;
+
+    public interface IReader
+    {
+        IList<ICommand> ReadCommands();
+    }
+}
