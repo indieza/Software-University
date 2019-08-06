651bb0d148f46c4c83e74cc605c3dc8227f9b42b
Simeon Valev <simeonvalev00@gmail.com>
2019-08-06 18:28:00 +0300

    Unit Tests - Done!!! - 100/100

--- C# OOP - 2019 Year/02. Exams/03. C# OOP Exam - 14 April 2019/Unit Tests/Telecom.Tests/Tests.cs
+++ C# OOP - 2019 Year/02. Exams/03. C# OOP Exam - 14 April 2019/Unit Tests/Telecom.Tests/Tests.cs
@@ -1,8 +1,68 @@
 namespace Telecom.Tests
 {
     using NUnit.Framework;
+    using System;
 
+    [TestFixture]
     public class Tests
     {
+        private Phone phone;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.phone = new Phone("Make", "Model");
+        }
+
+        [Test]
+        public void Test_Constructure()
+        {
+            Assert.AreEqual("Make", this.phone.Make);
+            Assert.AreEqual("Model", this.phone.Model);
+            Assert.AreEqual(0, this.phone.Count);
+        }
+
+        [Test]
+        public void Test_Make()
+        {
+            Assert.Throws<ArgumentException>(() => new Phone(null, "Model"));
+        }
+
+        [Test]
+        public void Test_Model()
+        {
+            Assert.Throws<ArgumentException>(() => new Phone("Make", null));
+        }
+
+        [Test]
+        public void Test_Add_Contact_Exception()
+        {
+            this.phone.AddContact("Name", "Phone");
+            Assert.Throws<InvalidOperationException>(() => this.phone.AddContact("Name", "Phone"));
+        }
+
+        [Test]
+        public void Test_Add_Contact()
+        {
+            this.phone.AddContact("Name", "Phone");
+            this.phone.AddContact("Name1", "Phone1");
+
+            Assert.AreEqual(2, this.phone.Count);
+        }
+
+        [Test]
+        public void Test_Call_Exception()
+        {
+            this.phone.AddContact("Name", "Phone");
+            Assert.Throws<InvalidOperationException>(() => this.phone.Call("Name1"));
+        }
+
+        [Test]
+        public void Test_Call()
+        {
+            this.phone.AddContact("Name", "Phone");
+            this.phone.AddContact("Name1", "Phone1");
+            Assert.AreEqual("Calling Name - Phone...", this.phone.Call("Name"));
+        }
     }
 }
