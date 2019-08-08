bf5911cd9cb8c54338c024e771c9e569c52598ae
Simeon Valev <simeonvalev00@gmail.com>
2019-08-08 18:07:34 +0300

    Extract Unit Tests

---
+++ C# OOP - 2019 Year/02. Exams/07. C# OOP Demo Exam - 04 August 2019/Unit Tests/TheRace/UnitMotorcycle.cs
@@ -0,0 +1,18 @@
+﻿namespace TheRace
+{
+    public class UnitMotorcycle
+    {
+        public UnitMotorcycle(string model, int horsePower, double cubicCentimeters)
+        {
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.CubicCentimeters = cubicCentimeters;
+        }
+
+        public string Model { get; }
+
+        public int HorsePower { get; }
+
+        public double CubicCentimeters { get; }
+    }
+}
