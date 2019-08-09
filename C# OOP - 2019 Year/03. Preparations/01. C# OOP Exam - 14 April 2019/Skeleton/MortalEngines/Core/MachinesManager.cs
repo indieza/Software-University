7ac02c0d7bd34fb82c7720353efabd61ed4d1180
Simeon Valev <simeonvalev00@gmail.com>
2019-08-09 18:08:36 +0300

    Extract Structure

---
+++ C# OOP - 2019 Year/03. Preparations/01. C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Core/MachinesManager.cs
@@ -0,0 +1,52 @@
+﻿namespace MortalEngines.Core
+{
+    using Contracts;
+
+    public class MachinesManager : IMachinesManager
+    {
+        public string HirePilot(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ManufactureTank(string name, double attackPoints, double defensePoints)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ManufactureFighter(string name, double attackPoints, double defensePoints)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string EngageMachine(string selectedPilotName, string selectedMachineName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AttackMachines(string attackingMachineName, string defendingMachineName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string PilotReport(string pilotReporting)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string MachineReport(string machineName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ToggleFighterAggressiveMode(string fighterName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ToggleTankDefenseMode(string tankName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
