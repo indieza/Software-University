335412cffcc3dd9046996cc507341e30fcc6e351
Simeon Valev <simeonvalev00@gmail.com>
2019-08-10 19:31:36 +0300

    Extract Unit Tests

---
+++ C# OOP - 2019 Year/03. Preparations/07. C# OOP Demo Exam - 04 August 2019/Unit Tests/TheRace/UnitRider.cs
@@ -0,0 +1,29 @@
+﻿namespace TheRace
+{
+    using System;
+
+    public class UnitRider
+    {
+        private string name;
+
+        public UnitRider(string name, UnitMotorcycle motorcycle)
+        {
+            this.Name = name;
+            this.Motorcycle = motorcycle;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value ?? throw new ArgumentNullException(nameof(Name), "Name cannot be null!");
+            }
+        }
+
+        public UnitMotorcycle Motorcycle { get; }
+    }
+}
