29bae3fc6d2805f1b0684d821850c42944a51b6f
Simeon Valev <simeonvalev00@gmail.com>
2019-09-04 18:46:40 +0300

    Last 2 Problems - Left - Need DB

--- MS SQL - септември 2019/01. Homeworks/04. Built-in functions - Exercises/Queries.sql
+++ MS SQL - септември 2019/01. Homeworks/04. Built-in functions - Exercises/Queries.sql
@@ -68,3 +68,34 @@ ORDER BY IsoCode;
     FROM Peaks, Rivers
    WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
 ORDER BY Mix;
+
+USE Diablo;
+
+  SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+    FROM Games
+   WHERE DATEPART(Year, [Start]) BETWEEN 2011 AND 2012
+ORDER BY [Start], [Name];
+
+  SELECT Username, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
+    FROM Users
+ORDER BY [Email Provider], Username;
+
+  SELECT Username, IpAddress
+    FROM Users
+   WHERE IpAddress LIKE '___.1_%._%.___'
+ORDER BY Username;
+
+  SELECT [Name], 
+    CASE
+  	WHEN DATEPART(HOUR, [Start]) >= 0 AND  DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+  	WHEN DATEPART(HOUR, [Start]) >= 12 AND  DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+  	ELSE 'Evening'
+    END AS [Part of the Day],
+    CASE
+  	WHEN Duration <= 3 THEN 'Extra Short'
+  	WHEN Duration >= 4 AND Duration <= 6 THEN 'Short'
+  	WHEN Duration > 6 THEN 'Long'
+  	ELSE 'Extra Long'
+    END AS [DurationName]
+    FROM Games
+ORDER BY [Name], [DurationName], [Part of the Day]
