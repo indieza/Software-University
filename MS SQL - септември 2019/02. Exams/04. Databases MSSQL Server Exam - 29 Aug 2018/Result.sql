2c420f24581b5ea208624fce6fc5ff15ae1ac1ac
Simeon Valev <simeonvalev00@gmail.com>
2019-09-06 15:51:48 +0300

    Upload Part Of The Solution

--- MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
+++ MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
@@ -53,3 +53,48 @@ CREATE TABLE Shifts
 
 ALTER TABLE Shifts
 ADD CONSTRAINT CHK_CheckDates CHECK(CheckIn < CheckOut);
+
+INSERT INTO Employees([FirstName], [LastName], [Phone], [Salary]) VALUES
+('Stoyan', 'Petrov', '888-785-8573', 500.25),
+('Stamat', 'Nikolov', '789-613-1122', 999995.25),
+('Evgeni', 'Petkov', '645-369-9517', 1234.51),
+('Krasimir', 'Vidolov', '321-471-9982',	50.25);
+
+INSERT INTO Items([Name], [Price], [CategoryId]) VALUES
+('Tesla battery', 154.25, 8),
+('Chess', 30.25, 8),
+('Juice', 5.32,	1),
+('Glasses',	10,	8),
+('Bottle of water',	1, 1);
+
+UPDATE Items
+   SET Price *= 1.27
+ WHERE CategoryId IN (1, 2, 3);
+
+DELETE OrderItems
+ WHERE OrderId = 48;
+DELETE Orders
+ WHERE Id = 48;
+
+  SELECT Id, FirstName
+    FROM Employees
+   WHERE Salary > 6500
+ORDER BY FirstName, Id;
+
+  SELECT FirstName + ' ' + LastName AS [Full Name], Phone
+    FROM Employees
+   WHERE Phone LIKE '3%'
+ORDER BY FirstName, Phone;
+
+  SELECT FirstName, LastName, COUNT(o.Id) AS [Count]
+    FROM Employees AS e
+    JOIN Orders AS o ON e.Id = o.EmployeeId
+GROUP BY FirstName, LastName
+ORDER BY [Count] DESC, E.FirstName;
+
+  SELECT e.FirstName, e.LastName, AVG(DATEDIFF(HOUR, s.CheckIn, s.CheckOut)) AS [Work hours]
+    FROM Employees AS e
+    JOIN Shifts AS s ON e.Id = s.EmployeeId
+GROUP BY e.FirstName, e.LastName, e.Id
+  HAVING AVG(DATEDIFF(HOUR, s.CheckIn, s.CheckOut)) > 7
+ORDER BY [Work hours] DESC, e.Id;
