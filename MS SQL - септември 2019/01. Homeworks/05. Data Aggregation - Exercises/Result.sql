b50dbd1055445c19a0812fd1de1c989126e85b36
Simeon Valev <simeonvalev00@gmail.com>
2019-09-08 18:17:33 +0300

    First 9 Problems - DONE!!! - 900/900

--- MS SQL - септември 2019/01. Homeworks/05. Data Aggregation - Exercises/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/05. Data Aggregation - Exercises/Result.sql
@@ -3,10 +3,10 @@ USE Gringotts;
 SELECT COUNT(*) AS [Count]
   FROM WizzardDeposits;
 
-SELECT MAX(MagicWandSize) AS [LongestMagicWand]
+SELECT MAX(MagicWandSize) AS LongestMagicWand
   FROM WizzardDeposits;
 
-  SELECT DepositGroup, MAX(MagicWandSize) AS [LongestMagicWand]
+  SELECT DepositGroup, MAX(MagicWandSize)
     FROM WizzardDeposits
 GROUP BY DepositGroup;
 
@@ -15,12 +15,11 @@ GROUP BY DepositGroup;
 GROUP BY DepositGroup
 ORDER BY AVG(MagicWandSize);
 
-
-  SELECT DepositGroup, SUM(DepositAmount) AS [TotalSum]
+  SELECT DepositGroup, SUM(DepositAmount)
     FROM WizzardDeposits
 GROUP BY DepositGroup;
 
-  SELECT DepositGroup, SUM(DepositAmount) AS [TotalSum]
+  SELECT DepositGroup, SUM(DepositAmount)
     FROM WizzardDeposits
    WHERE MagicWandCreator = 'Ollivander family'
 GROUP BY DepositGroup;
@@ -30,26 +29,25 @@ GROUP BY DepositGroup;
    WHERE MagicWandCreator = 'Ollivander family'
 GROUP BY DepositGroup
   HAVING SUM(DepositAmount) < 150000
-ORDER BY TotalSum DESC;
+ORDER BY [TotalSum] DESC;
 
-  SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge)
+  SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS [MinDepositCharge]
     FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 ORDER BY MagicWandCreator, DepositGroup;
 
-SELECT GroupAges.AgeGroup, COUNT(GroupAges.AgeGroup)
-  FROM
+  SELECT [Ages].AgeGroup, COUNT(Ages.AgeGroup)
+    FROM
 (
-SELECT
-  CASE
-	WHEN Age >= 0 AND Age <= 10 THEN '[0-10]'
-	WHEN Age >= 11 AND Age <= 20 THEN '[11-20]'
-	WHEN Age >= 21 AND Age <= 30 THEN '[21-30]'
-	WHEN Age >= 31 AND Age <= 40 THEN '[31-40]'
-	WHEN Age >= 41 AND Age <= 50 THEN '[41-50]'
-	WHEN Age >= 51 AND Age <= 60 THEN '[51-60]'
-	ELSE '[61+]'
+  SELECT 
+    CASE
+  		WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+  		WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+  		WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+  		WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+  		WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+  		WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+  	ELSE '[61+]'
   END AS [AgeGroup]
-    FROM WizzardDeposits
-    ) AS [GroupAges]
-GROUP BY GroupAges.AgeGroup
+    FROM WizzardDeposits) AS [Ages]
+GROUP BY Ages.AgeGroup;
