f1ce1cfd1c26279b301dfc4eabebd83f7df33815
Simeon Valev <simeonvalev00@gmail.com>
2019-09-10 16:53:28 +0300

    Problem 9 - DONE!!! - 100/100

--- MS SQL - септември 2019/01. Homeworks/06. Table Relations - Exercises/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/06. Table Relations - Exercises/Result.sql
@@ -212,3 +212,13 @@ CREATE TABLE Agenda
 	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID) NOT NULL,
 	CONSTRAINT PK_Agenda PRIMARY KEY (StudentID, SubjectID)
 );
+
+USE [Geography];
+
+  SELECT *
+    FROM (
+  SELECT MountainRange, p.PeakName, p.Elevation
+    FROM Mountains AS m
+    JOIN Peaks AS p ON p.MountainId = m.Id) AS g
+   WHERE MountainRange = 'Rila'
+ORDER BY g.Elevation DESC;
