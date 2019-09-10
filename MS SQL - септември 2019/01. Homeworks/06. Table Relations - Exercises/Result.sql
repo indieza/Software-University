b81656ce6897c5a1de4ec3fc3e9d4b2d2043f5d4
Simeon Valev <simeonvalev00@gmail.com>
2019-09-10 15:02:15 +0300

    Problem 3 - DONE!!! - 100/100

--- MS SQL - септември 2019/01. Homeworks/06. Table Relations - Exercises/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/06. Table Relations - Exercises/Result.sql
@@ -64,3 +64,49 @@ INSERT INTO Models(MoDelID, [Name], ManufacturerID) VALUES
 SELECT *
   FROM Manufacturers AS m
   JOIN Models AS ml ON ml.ManufacturerID = m.ManufacturerID;
+
+CREATE DATABASE ManyToMany;
+
+USE ManyToMany;
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY NOT NULL,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID) NOT NULL,
+	ExamID INT FOREIGN KEY REFERENCES Exams(ExamID) NOT NULL,
+	CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+);
+
+INSERT INTO Students([Name]) VALUES
+('Mila'),
+('Toni'),
+('Ron');
+
+INSERT INTO Exams([ExamID], [Name]) VALUES
+(101, 'SpringMVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g');
+
+INSERT INTO StudentsExams([StudentID], [ExamID]) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
+
+SELECT *
+  FROM StudentsExams AS se
+  JOIN Students AS s ON s.StudentID = se.StudentID
+  JOIN Exams AS e ON e.ExamID = se.ExamID;
