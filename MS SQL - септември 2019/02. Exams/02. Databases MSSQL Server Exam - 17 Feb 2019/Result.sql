b1315904a368818d763627620f7104015e30a3ef
Simeon Valev <simeonvalev00@gmail.com>
2019-09-11 18:44:29 +0300

    Problem 16 - DONE!!!

--- MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
@@ -143,3 +143,9 @@ ORDER BY FirstName, LastName;
     FULL JOIN StudentsSubjects AS ss ON ss.StudentId = s.Id
    WHERE ss.StudentId IS NULL
 ORDER BY [Full Name];
+
+  SELECT s.[Name], AVG(ss.Grade) AS [AverageGrade]
+    FROM Subjects AS s
+    JOIN StudentsSubjects AS ss ON ss.SubjectId = s.Id
+GROUP BY s.[Name], s.Id
+ORDER BY s.Id;
