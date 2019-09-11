51caca7e554248cd7ab0d478873c6861746189b2
Simeon Valev <simeonvalev00@gmail.com>
2019-09-11 19:19:44 +0300

    Problem 17 - DONE!!! - DO IT AGAIN FOR SKILLS

--- MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
@@ -149,3 +149,20 @@ ORDER BY [Full Name];
     JOIN StudentsSubjects AS ss ON ss.SubjectId = s.Id
 GROUP BY s.[Name], s.Id
 ORDER BY s.Id;
+
+  SELECT res.[Quarter], res.SubjectName, COUNT(res.StudentId) AS StudentsCount
+    FROM (
+  SELECT s.[Name] AS SubjectName, se.StudentId,
+    CASE
+  		WHEN DATEPART(MONTH, e.[Date]) BETWEEN 1 AND 3 THEN 'Q1'
+  		WHEN DATEPART(MONTH, e.[Date]) BETWEEN 4 AND 6 THEN 'Q2'
+  		WHEN DATEPART(MONTH, e.[Date]) BETWEEN 7 AND 9 THEN 'Q3'
+  		WHEN DATEPART(MONTH, e.[Date]) BETWEEN 10 AND 12 THEN 'Q4'
+  	ELSE 'TBA'
+     END AS [Quarter]
+    FROM Exams AS e
+    JOIN Subjects AS s ON s.Id = e.SubjectId
+    JOIN StudentsExams AS se ON se.ExamId = e.Id
+   WHERE se.Grade >= 4.00) AS res
+GROUP BY res.SubjectName, res.[Quarter]
+ORDER BY res.[Quarter];
