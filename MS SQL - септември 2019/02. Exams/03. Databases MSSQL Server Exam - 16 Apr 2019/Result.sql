e94cfc8bc56ba08cf42ac547ab2481e88152f7ac
Simeon Valev <simeonvalev00@gmail.com>
2019-09-12 18:26:49 +0300

    Problem 14 - DONE!!!

--- MS SQL - септември 2019/02. Exams/03. Databases MSSQL Server Exam - 16 Apr 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/03. Databases MSSQL Server Exam - 16 Apr 2019/Result.sql
@@ -126,3 +126,15 @@ ORDER BY p.PassportId, p.[Address];
     FULL JOIN Tickets AS t ON t.PassengerId = p.Id
 GROUP BY p.FirstName, p.LastName
 ORDER BY[Total Trips] DESC, p.FirstName, p.LastName;
+
+  SELECT p.FirstName + ' ' + p.LastName AS [Full Name], 
+         pl.[Name] AS [Plane Name],
+		 f.Origin + ' - ' + f.Destination AS Trip,
+		 lt.[Type]
+    FROM Passengers AS p
+    JOIN Tickets AS t ON t.PassengerId = p.Id
+	JOIN Flights AS f ON f.Id = t.FlightId
+	JOIN Planes AS pl ON pl.Id = f.PlaneId
+	JOIN Luggages AS l ON l.Id = t.LuggageId
+	JOIN LuggageTypes AS lt ON lt.Id = l.LuggageTypeId
+ORDER BY [Full Name], pl.[Name], f.Origin, f.Destination, lt.[Type];
