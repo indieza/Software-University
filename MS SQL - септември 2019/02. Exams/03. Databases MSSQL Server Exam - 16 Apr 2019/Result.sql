fe8632205d6aec966930d9e2c9cef0d695ab3679
Simeon Valev <simeonvalev00@gmail.com>
2019-09-12 17:17:54 +0300

    Third Select - DONE!!!

--- MS SQL - септември 2019/02. Exams/03. Databases MSSQL Server Exam - 16 Apr 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/03. Databases MSSQL Server Exam - 16 Apr 2019/Result.sql
@@ -85,3 +85,9 @@ ORDER BY Origin, Destination;
     FROM Planes
    WHERE [Name] LIKE '%tr%'
 ORDER BY Id, [Name], Seats, [Range];
+
+  SELECT f.Id, SUM(t.Price) AS [Price]
+    FROM Flights AS f
+    JOIN Tickets AS t ON t.FlightId = f.Id
+GROUP BY f.Id
+ORDER BY Price DESC, f.Id;
