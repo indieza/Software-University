6a16afd8169f364ec286d66bcb7260cb05ad72dc
Simeon Valev <simeonvalev00@gmail.com>
2019-09-13 16:37:07 +0300

    Problem 13 - DONE!!!

--- MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
+++ MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
@@ -124,3 +124,14 @@ ORDER BY e.Id;
    WHERE DATEDIFF(HOUR, s.CheckIn, s.CheckOut) < 4
 GROUP BY e.Id, e.FirstName, e.LastName
 ORDER BY e.Id;
+
+  SELECT TOP(10) e.FirstName + ' ' + e.LastName AS [Full Name],
+                 SUM(i.Price * oi.Quantity) AS [Total Price],
+				 SUM(oi.Quantity) AS Items
+    FROM Employees AS e
+    JOIN Orders AS o ON o.EmployeeId = e.Id
+    JOIN OrderItems AS oi ON oi.OrderId = o.Id
+    JOIN Items AS i ON i.Id = oi.ItemId
+   WHERE o.[DateTime] < '2018-06-15'
+GROUP BY e.FirstName, e.LastName
+ORDER BY [Total Price] DESC, Items DESC;
