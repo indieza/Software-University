a47265c5e7fa541a2cd0bceeca517caea5dbc84a
Simeon Valev <simeonvalev00@gmail.com>
2019-09-13 15:51:00 +0300

    Forth Select - DONE!!!

--- MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
+++ MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
@@ -89,3 +89,10 @@ ORDER BY FirstName, Phone;
     JOIN Orders AS o ON o.EmployeeId = e.Id
 GROUP BY e.FirstName, e.LastName
 ORDER BY [Count] DESC, e.FirstName;
+
+  SELECT e.FirstName, e.LastName, AVG(DATEDIFF(HOUR, s.CheckIn, s.CheckOut)) AS [Work hours]
+    FROM Employees AS e
+    JOIN Shifts AS s ON s.EmployeeId = e.Id
+GROUP BY e.FirstName, e.LastName, e.Id
+  HAVING AVG(DATEDIFF(HOUR, s.CheckIn, s.CheckOut)) > 7
+ORDER BY [Work hours] DESC, e.Id;
