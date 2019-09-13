a0a13b91d2792dd0f44eaf7311ecfb1d7ac2eec2
Simeon Valev <simeonvalev00@gmail.com>
2019-09-13 15:35:36 +0300

    Delete - DONE!!!

--- MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
+++ MS SQL - септември 2019/02. Exams/04. Databases MSSQL Server Exam - 29 Aug 2018/Result.sql
@@ -66,3 +66,10 @@ INSERT INTO Items([Name], [Price], [CategoryId]) VALUES
 UPDATE Items
    SET Price *= 1.27
  WHERE CategoryId IN (1, 2, 3);
+
+DELETE
+  FROM OrderItems
+ WHERE OrderId = 48;
+DELETE
+  FROM Orders
+ WHERE Id = 48;
