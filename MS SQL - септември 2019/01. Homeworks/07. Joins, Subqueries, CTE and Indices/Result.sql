08fd2c1a5a9aa9c79c8a8ff9b4c7e13c0f745344
Simeon Valev <simeonvalev00@gmail.com>
2019-09-15 17:24:14 +0300

    Problem 2 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
@@ -2,3 +2,9 @@
     FROM Employees AS e
     JOIN Addresses AS a ON a.AddressID = e.AddressID
 ORDER BY a.AddressID;
+
+  SELECT TOP(50) e.FirstName, e.LastName, t.[Name], a.AddressText
+    FROM Employees AS e
+    JOIN Addresses AS a ON a.AddressID = e.AddressID
+    JOIN Towns AS t ON t.TownID = a.TownID
+ORDER BY e.FirstName, e.LastName;
