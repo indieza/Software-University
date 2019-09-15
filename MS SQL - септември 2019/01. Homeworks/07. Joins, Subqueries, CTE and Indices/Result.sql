88e86c16df9ea9283d5eff9cc5fca3cf33e5e4f5
Simeon Valev <simeonvalev00@gmail.com>
2019-09-15 17:27:58 +0300

    Problem 4 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
@@ -14,3 +14,9 @@ ORDER BY e.FirstName, e.LastName;
     JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
    WHERE d.[Name] = 'Sales'
 ORDER BY e.EmployeeID;
+
+  SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.[Name]
+    FROM Employees AS e
+    JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+   WHERE e.Salary > 15000
+ORDER BY e.DepartmentID;
