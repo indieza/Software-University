bf1e35626a0f1e8d920f7322c5845ec0d4443be0
Simeon Valev <simeonvalev00@gmail.com>
2019-09-15 18:43:18 +0300

    Problem 17 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/07. Joins, Subqueries, CTE and Indices/Result.sql
@@ -107,3 +107,15 @@ SELECT COUNT(*) AS [CountryCode]
   FROM Countries AS c
   FULL JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
  WHERE mc.MountainId IS NULL;
+
+  SELECT TOP(5) c.CountryName,
+         MAX(p.Elevation) AS [HighestPeakElevation],
+		 MAX(r.[Length]) AS [LongestRiverLength]
+    FROM Countries AS c
+    JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
+    JOIN Mountains AS m ON m.Id = mc.MountainId
+    JOIN Peaks AS p ON p.MountainId = mc.MountainId
+    JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
+    JOIN Rivers AS r ON r.Id = cr.RiverId
+GROUP BY c.CountryName
+ORDER BY [HighestPeakElevation] DESC, [LongestRiverLength] DESC, c.CountryName;
