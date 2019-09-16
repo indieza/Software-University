64cf4324062a7e37341b21c1b7f1baf0e0baa1d9
Simeon Valev <simeonvalev00@gmail.com>
2019-09-16 16:13:46 +0300

    Problem 2 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
@@ -5,3 +5,11 @@ SELECT FirstName, LastName
  WHERE Salary > 35000
 
 EXEC dbo.usp_GetEmployeesSalaryAbove35000
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber @Number DECIMAL(18,4)
+    AS
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE Salary >= @Number
+
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber @Number = 48100
