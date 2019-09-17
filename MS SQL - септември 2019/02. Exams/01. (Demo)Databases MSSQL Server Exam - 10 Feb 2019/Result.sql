cbe7ae1bc9e64cd7950e0154c9a2d98a5bae9753
Simeon Valev <simeonvalev00@gmail.com>
2019-09-17 19:05:10 +0300

    Problem 18 - DONE!!!

--- MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Result.sql
@@ -141,3 +141,18 @@ ORDER BY [JourneysCount] DESC, Planets.PlanetName;
     JOIN Spaceports AS s ON s.Id = j.DestinationSpaceportId
     JOIN Planets AS p ON p.Id = s.PlanetId
 ORDER BY DATEDIFF(SECOND, j.JourneyStart, j.JourneyEnd);
+
+CREATE FUNCTION dbo.udf_GetColonistsCount(@PlanetName VARCHAR (30))
+RETURNS INT
+AS
+BEGIN
+	RETURN (SELECT COUNT(*)
+             FROM Planets AS p
+             JOIN Spaceports AS s ON s.PlanetId = p.Id
+             JOIN Journeys AS j ON j.DestinationSpaceportId = s.Id
+             JOIN TravelCards AS tc ON tc.JourneyId = j.Id
+             JOIN Colonists AS c ON c.Id = tc.ColonistId
+            WHERE p.[Name] = @PlanetName)
+END
+
+SELECT dbo.udf_GetColonistsCount('Otroyphus')
