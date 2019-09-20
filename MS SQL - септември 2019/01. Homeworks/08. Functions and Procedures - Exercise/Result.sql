33100d066dcf8c54db50c54015562d3fdc9098a8
Simeon Valev <simeonvalev00@gmail.com>
2019-09-20 19:55:03 +0300

    Problem 16 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
@@ -258,3 +258,32 @@ SET
 WHERE Id = 1
 
 SELECT* FROM dbo.NotificationEmails ne
+
+-- Problem 16
+CREATE PROCEDURE usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(15, 4))
+AS
+BEGIN TRANSACTION
+
+DECLARE @targetAccountId INT = (SELECT a.Id FROM dbo.Accounts a WHERE a.Id = @AccountId)
+
+IF(@MoneyAmount < 0 OR @MoneyAmount IS NULL)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid amount of money', 16, 1)
+	RETURN
+END
+
+IF(@targetAccountId IS NULL)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid Id Parameter', 16, 2)
+	RETURN
+END
+
+UPDATE dbo.Accounts
+SET
+    dbo.Accounts.Balance += @MoneyAmount
+WHERE dbo.Accounts.Id = @targetAccountId
+COMMIT
+
+EXEC usp_DepositMoney 1, 10
