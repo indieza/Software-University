b1859c1b3b48de9a7760b07958cf30f2a0e70706
Simeon Valev <simeonvalev00@gmail.com>
2019-09-20 18:55:11 +0300

    Problem 15 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
@@ -211,3 +211,31 @@ SELECT *
 
 SELECT *
   FROM Logs
+
+-- Problem 15
+CREATE TABLE NotificationEmails
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Recipient INT FOREIGN KEY REFERENCES Accounts(Id),
+	[Subject] VARCHAR(50),
+	Body VARCHAR(MAX)
+);
+
+CREATE TRIGGER tr_LogEmail ON Logs FOR INSERT
+    AS
+DECLARE @accountId INT = (SELECT TOP(1) AccountId FROM inserted)
+DECLARE @oldSum DECIMAL(15, 2) = (SELECT TOP(1) OldSum FROM inserted)
+DECLARE @newSum DECIMAL(15, 2) = (SELECT TOP(1) NewSum FROM inserted)
+
+INSERT INTO NotificationEmails(Recipient, [Subject], Body) VALUES
+(
+ @accountId,
+ 'Balance change for account: ' + CAST(@accountId AS VARCHAR(20)),
+ 'On ' + CONVERT(VARCHAR(30), GETDATE(), 103) +
+ ' your balance was changed from ' + CAST(@oldSum AS VARCHAR(20)) +
+ ' to' + CAST(@newSum AS VARCHAR(20)) + '.'
+);
+
+SELECT * FROM Accounts WHERE Id = 1
+
+UPDATE Accounts SET Balance += 10 WHERE Id = 1
