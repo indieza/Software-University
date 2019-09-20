eac841fe7062e38ac01e8676ea72aa2cada1a23a
Simeon Valev <simeonvalev00@gmail.com>
2019-09-20 18:36:44 +0300

    Problem 14 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
@@ -181,3 +181,33 @@ RETURN
 
 SELECT dbo.ufn_CashInUsersGames('Lily Stargazer')
 SELECT dbo.ufn_CashInUsersGames('Love in a mist')
+
+-- Problem 14
+CREATE TABLE Logs
+(
+	Id INT PRIMARY KEY IDENTITY,
+	AccountId int FOREIGN KEY REFERENCES Accounts(Id),
+	OldSum DECIMAL(15, 2),
+	NewSum DECIMAL(15, 2)
+);
+
+CREATE TRIGGER tr_InsertAccountsInfo ON dbo.Accounts FOR UPDATE
+    AS
+DECLARE @newSum DECIMAL(15, 2) = (SELECT Balance FROM inserted)
+DECLARE @oldSum DECIMAL(15, 2) = (SELECT Balance FROM deleted)
+DECLARE @accoountId INT = (SELECT Id FROM inserted)
+
+INSERT INTO Logs(AccountId, NewSum, OldSum) VALUES
+(@accoountId, @newSum, @oldSum)
+
+
+UPDATE dbo.Accounts
+   SET Balance += 10
+ WHERE Id = 1
+
+SELECT *
+  FROM Accounts
+ WHERE Id = 1
+
+SELECT *
+  FROM Logs
