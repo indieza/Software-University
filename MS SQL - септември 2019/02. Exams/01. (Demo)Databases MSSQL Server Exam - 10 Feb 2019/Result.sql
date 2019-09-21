8f7aaba4f8b16aa437001ad4594601005f5fe8ff
Simeon Valev <simeonvalev00@gmail.com>
2019-09-21 20:15:42 +0300

    Problem 20 - DONE!!!

--- MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Result.sql
@@ -196,3 +196,35 @@ EXEC usp_ChangeJourneyPurpose 1, 'Technical'
 SELECT * FROM Journeys
 EXEC usp_ChangeJourneyPurpose 2, 'Educational'
 EXEC usp_ChangeJourneyPurpose 196, 'Technical'
+
+-- 20. Deleted Journeys  (Id, JourneyStart, JourneyEnd, Purpose, DestinationSpaceportId, SpaceshipId)
+CREATE TABLE DeletedJourneys
+(
+	Id INT,
+	JourneyStart DATETIME,
+	JourneyEnd DATETIME,
+	Purpose VARCHAR(11),
+	DestinationSpaceportId INT,
+	SpaceshipId INT
+);
+
+CREATE TRIGGER tr_DeletedJourneys ON [dbo].[Journeys] FOR DELETE
+AS
+BEGIN
+	INSERT INTO [dbo].[DeletedJourneys]
+	(
+	    [Id],
+	    [JourneyStart],
+	    [JourneyEnd],
+	    [Purpose],
+	    [DestinationSpaceportId],
+	    [SpaceshipId]
+	)
+	SELECT * FROM [DELETED] AS d
+END
+
+DELETE FROM TravelCards
+WHERE JourneyId =  1
+
+DELETE FROM Journeys
+WHERE Id =  1
