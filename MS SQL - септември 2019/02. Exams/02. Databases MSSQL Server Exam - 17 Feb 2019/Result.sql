4ec715257a9cffad7311b9425fa9309c9b6edcc1
Simeon Valev <simeonvalev00@gmail.com>
2019-09-21 20:28:44 +0300

    Problem 20 - DONE!!!

--- MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
+++ MS SQL - септември 2019/02. Exams/02. Databases MSSQL Server Exam - 17 Feb 2019/Result.sql
@@ -222,3 +222,34 @@ DELETE
 EXEC usp_ExcludeFromSchool 1
 SELECT COUNT(*) FROM Students
 EXEC usp_ExcludeFromSchool 301
+
+CREATE TABLE ExcludedStudents
+(
+	StudentId INT,
+	StudentName NVARCHAR(100)
+);
+
+CREATE TRIGGER tr_DeletedStudents ON [dbo].[Students] FOR DELETE
+AS
+BEGIN
+	INSERT INTO [dbo].[ExcludedStudents]
+	(
+	    [StudentId],
+	    [StudentName]
+	)
+	SELECT [Id], [FirstName] + ' ' + [LastName] AS [StudentName] FROM [DELETED] AS d
+END
+
+DELETE FROM StudentsExams
+WHERE StudentId = 1
+
+DELETE FROM StudentsTeachers
+WHERE StudentId = 1
+
+DELETE FROM StudentsSubjects
+WHERE StudentId = 1
+
+DELETE FROM Students
+WHERE Id = 1
+
+SELECT * FROM ExcludedStudents
