b331f0851114d48145146c940f53b1951a95566f
Simeon Valev <simeonvalev00@gmail.com>
2019-09-21 14:12:05 +0300

    Problem 18 - DONE!!!

--- MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
+++ MS SQL - септември 2019/01. Homeworks/08. Functions and Procedures - Exercise/Result.sql
@@ -314,3 +314,30 @@ WHERE Id = @AccountId
 COMMIT
 
 EXEC [dbo].usp_WithdrawMoney 1, 10
+
+-- Problem 18
+CREATE PROCEDURE usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(15, 4))
+AS
+BEGIN TRANSACTION
+DECLARE @targetSender INT = (SELECT Id FROM [dbo].[Accounts] AS a WHERE a.[Id] = @SenderId)
+DECLARE @targetReciver INT = (SELECT Id FROM [dbo].[Accounts] AS a WHERE a.[Id] = @ReceiverId)
+
+IF(@targetReciver IS NULL OR @targetSender IS NULL)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid Id Parameter', 16, 1)
+	RETURN
+END
+
+IF(@Amount < 0)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid amount of money', 16, 2)
+	RETURN
+END
+
+EXEC dbo.usp_WithdrawMoney @targetSender, @Amount
+EXEC dbo.usp_DepositMoney @targetReciver, @Amount
+COMMIT
+
+EXEC dbo.usp_TransferMoney 5, 1, 5000
