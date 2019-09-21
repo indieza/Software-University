7a6b32d83f80755c86a892617a18eb3da6ca95f1
Simeon Valev <simeonvalev00@gmail.com>
2019-09-21 15:08:28 +0300

    Problem 5 - DONE!!!

---
+++ MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Result.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE ColonialJourney;
+
+USE ColonialJourney;
+
+CREATE TABLE Planets
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Spaceports
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	PlanetId INT FOREIGN KEY REFERENCES Planets(Id) NOT NULL
+);
+
+CREATE TABLE Spaceships
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	Manufacturer VARCHAR(30) NOT NULL,
+	LightSpeedRate INT DEFAULT(0)
+)
+
+CREATE TABLE Colonists
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName VARCHAR(20) NOT NULL,
+	LastName VARCHAR(20) NOT NULL,
+	Ucn VARCHAR(20) NOT NULL UNIQUE,
+	BirthDate DATE NOT NULL
+);
+
+CREATE TABLE Journeys
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	JourneyStart DATETIME NOT NULL,
+	JourneyEnd DATETIME NOT NULL,
+	Purpose VARCHAR(11) NOT NULL CHECK(Purpose = 'Medical' OR
+									   Purpose = 'Technical' OR
+									   Purpose = 'Educational' OR
+									   Purpose = 'Military'),
+	DestinationSpaceportId INT FOREIGN KEY REFERENCES Spaceports([Id]) NOT NULL,
+	SpaceshipId INT FOREIGN KEY REFERENCES Spaceships([Id]) NOT NULL
+);
+
+CREATE TABLE TravelCards
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	CardNumber CHAR(10) NOT NULL UNIQUE,
+	JobDuringJourney VARCHAR(8) NOT NULL CHECK(JobDuringJourney = 'Pilot' OR
+									           JobDuringJourney = 'Engineer' OR
+											   JobDuringJourney = 'Trooper' OR
+											   JobDuringJourney = 'Cleaner' OR
+											   JobDuringJourney = 'Cook'),
+	ColonistId INT FOREIGN KEY REFERENCES Colonists([Id]) NOT NULL,
+	JourneyId INT FOREIGN KEY REFERENCES Journeys([Id]) NOT NULL
+);
+
+-- 02. Insert
+INSERT INTO [dbo].[Planets]([Name]) VALUES
+('Mars'),
+('Earth'),
+('Jupiter'),
+('Saturn');
+
+INSERT INTO Spaceships([Name], [Manufacturer], [LightSpeedRate]) VALUES
+('Golf', 'VW', 3),
+('WakaWaka', 'Wakanda', 4),
+('Falcon9', 'SpaceX', 1),
+('Bed', 'Vidolov', 6);
+
+-- 03. Update
+UPDATE Spaceships
+   SET LightSpeedRate += 1
+ WHERE Id BETWEEN 8 AND 12;
+
+-- 04. Delete
+DELETE TravelCards
+ WHERE JourneyId IN(1, 2, 3);
+
+DELETE Journeys
+ WHERE Id IN(1, 2, 3);
+
+ -- 05. Select all travel cards
+  SELECT CardNumber, JobDuringJourney
+    FROM TravelCards
+ORDER BY CardNumber;
