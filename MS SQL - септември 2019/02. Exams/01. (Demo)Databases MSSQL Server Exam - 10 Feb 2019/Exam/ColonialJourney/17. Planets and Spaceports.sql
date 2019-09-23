cb2945a0e5593dd8bc2c971409058257030341cc
Simeon Valev <simeonvalev00@gmail.com>
2019-09-23 18:03:37 +0300

    Problems 12, 13, 17 - DONE!!!

---
+++ MS SQL - септември 2019/02. Exams/01. (Demo)Databases MSSQL Server Exam - 10 Feb 2019/Exam/ColonialJourney/17. Planets and Spaceports.sql
@@ -0,0 +1,5 @@
+   SELECT [p].[Name], COUNT([s].[Name]) AS [Count]
+     FROM [dbo].[Planets] AS p
+LEFT JOIN [dbo].[Spaceports] AS [s] ON [p].[Id] = [s].[PlanetId]
+ GROUP BY [p].[Name]
+ ORDER BY [Count] DESC, [p].[Name];
