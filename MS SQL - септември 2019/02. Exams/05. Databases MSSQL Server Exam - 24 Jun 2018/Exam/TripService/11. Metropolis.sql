d6c55fd7c4d6480a1a359dd471011c9c5bc6d7d5
Simeon Valev <simeonvalev00@gmail.com>
2019-10-02 16:13:50 +0300

    Problems 11 To 15 - DONEE!!!

---
+++ MS SQL - септември 2019/02. Exams/05. Databases MSSQL Server Exam - 24 Jun 2018/Exam/TripService/11. Metropolis.sql
@@ -0,0 +1,8 @@
+  SELECT TOP(5) [c].[Id],
+         [c].[Name] AS [City],
+  	     [c].[CountryCode] AS [Country],
+  	     COUNT([a].[Id]) AS [Accounts]
+    FROM [dbo].[Cities] AS c
+    JOIN [dbo].[Accounts] AS [a] ON [c].[Id] = [a].[CityId]
+GROUP BY [c].[Id], [c].[Name], [c].[CountryCode]
+ORDER BY [Accounts] DESC;
---
+++ MS SQL - септември 2019/02. Exams/05. Databases MSSQL Server Exam - 24 Jun 2018/Exam/TripService/13. Lucrative Destinations.sql
@@ -0,0 +1,11 @@
+  SELECT TOP(10) [c].[Id],
+         [c].[Name],
+  	     SUM([h].[BaseRate] + [r].[Price]) AS [Total Revenue],
+  	     COUNT([t].[Id]) AS [Trips]
+    FROM [dbo].[Cities] AS c
+    JOIN [dbo].[Hotels] AS [h] ON [c].[Id] = [h].[CityId]
+    JOIN [dbo].[Rooms] AS [r] ON [h].[Id] = [r].[HotelId]
+    JOIN [dbo].[Trips] AS [t] ON [r].[Id] = [t].[RoomId]
+   WHERE DATEPART(YEAR, [t].[BookDate]) = 2016
+GROUP BY [c].[Id], [c].[Name]
+ORDER BY [Total Revenue] DESC, [Trips] DESC;
