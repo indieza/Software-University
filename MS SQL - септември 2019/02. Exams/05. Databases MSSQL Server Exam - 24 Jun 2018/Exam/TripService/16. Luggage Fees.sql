5c96c75577f6cc6b0e97fd6713919b93d1dce0db
Simeon Valev <simeonvalev00@gmail.com>
2019-10-04 18:10:12 +0300

    Problems 16, 17 AND 18 - DONE!!!

---
+++ MS SQL - септември 2019/02. Exams/05. Databases MSSQL Server Exam - 24 Jun 2018/Exam/TripService/16. Luggage Fees.sql
@@ -0,0 +1,11 @@
+  SELECT [t].[Id] AS [TripId],
+         SUM([at].[Luggage]) AS [Luggage],
+		 CAST(CASE
+				  WHEN SUM([at].[Luggage]) > 5 THEN '$' + CAST(SUM([at].[Luggage]) * 5 AS VARCHAR(50))
+				  ELSE '$0'
+			  END AS VARCHAR(50)) AS [Fee]
+    FROM [dbo].[Trips] AS t
+    JOIN [dbo].[AccountsTrips] AS [at] ON [t].[Id] = [at].[TripId]
+GROUP BY [t].[Id]
+  HAVING SUM([at].[Luggage]) > 0
+ORDER BY [Luggage] DESC;
