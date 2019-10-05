e100b3086cceb4660b596f9f96a2dcb3650de276
Simeon Valev <simeonvalev00@gmail.com>
2019-10-05 17:58:48 +0300

    Sh!t - DONE!!!

--- MS SQL - септември 2019/02. Exams/06. Databases MSSQL Server Exam - 22 October 2017/Exam/ReportService/14. Open-Closed Statistics.sql
+++ MS SQL - септември 2019/02. Exams/06. Databases MSSQL Server Exam - 22 October 2017/Exam/ReportService/14. Open-Closed Statistics.sql
@@ -1,10 +1,6 @@
   SELECT [e].[FirstName] + ' ' + [e].[LastName] AS [Name],
          [r].[CloseDate],
-  	     [r].[OpenDate],
-		 CASE
-			WHEN DATEPART(YEAR, [r].[OpenDate]) = 2015 AND DATEPART(YEAR, [r].[CloseDate]) = 2016 THEN 1
-		END AS [Name Closed Open Reports]
+  	     [r].[OpenDate]
     FROM [dbo].[Employees] AS e
     JOIN [dbo].[Reports] AS [r] ON [e].[Id] = [r].[EmployeeId]
    WHERE (DATEPART(YEAR, [r].[OpenDate]) = 2016 OR DATEPART(YEAR, [r].[CloseDate]) = 2016)
-ORDER BY [Name], [e].[Id];
