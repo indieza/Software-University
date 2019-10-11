66170d3b42e8d5080dfdb9dc5f2bf4ddceb32734
Simeon Valev <simeonvalev00@gmail.com>
2019-10-11 14:57:40 +0300

    Exam - DONE!!! - 100/100

---
+++ MS SQL - септември 2019/02. Exams/06. Exam Preparation 1/Exam/Airport/10. PSP.sql
@@ -0,0 +1,8 @@
+   SELECT [p].[Name],
+          [p].[Seats],
+   	      COUNT([t].[PassengerId]) AS [Passengers Count]
+     FROM [dbo].[Planes] AS p
+LEFT JOIN [dbo].[Flights] AS [f] ON [p].[Id] = [f].[PlaneId]
+LEFT JOIN [dbo].[Tickets] AS [t] ON [f].[Id] = [t].[FlightId]
+ GROUP BY [p].[Name], [p].[Seats]
+ ORDER BY [Passengers Count] DESC, [p].[Name], [p].[Seats];
