38c92b3a7c86d627f591994fec6a9e060df1f1f4
Simeon Valev <simeonvalev00@gmail.com>
2019-10-25 10:41:55 +0300

    Edit Problem 12 - 10/10

--- MS SQL - септември 2019/02. Exams/09. Databases MSSQL Server Exam - 20 Oct 2019/Exam/Service/12. Assign Employee.sql
+++ MS SQL - септември 2019/02. Exams/09. Databases MSSQL Server Exam - 20 Oct 2019/Exam/Service/12. Assign Employee.sql
@@ -1,25 +1,25 @@
 CREATE PROCEDURE usp_AssignEmployeeToReport(@EmployeeId INT, @ReportId INT)
 AS
 BEGIN
-	DECLARE @employeeDepartmentId INT = (SELECT [e].[DepartmentId]
-                                           FROM [dbo].[Employees] AS e
-                                          WHERE [e].[Id] = @EmployeeId)
+	DECLARE @employeeDepartment INT = (SELECT [e].[DepartmentId]
+                                         FROM [dbo].[Employees] AS e
+                                        WHERE [e].[Id] = @EmployeeId)
 
-	DECLARE @reportDepartmentId INT = (SELECT [e].[DepartmentId]
-                                         FROM [dbo].[Reports] AS r
-                                         JOIN [dbo].[Employees] AS [e] ON [r].[EmployeeId] = [e].[Id]
-                                        WHERE [r].[Id] = @ReportId)
+	DECLARE @reportDepartment INT = (SELECT [c].[DepartmentId]
+                                       FROM [dbo].[Reports] AS r
+                                       JOIN [dbo].[Categories] AS [c] ON [r].[CategoryId] = [c].[Id]
+                                      WHERE [r].[Id] = @ReportId)
 
-	IF(@employeeDepartmentId <> @reportDepartmentId)
+	IF(@employeeDepartment <> @reportDepartment)
 	BEGIN
 		RAISERROR('Employee doesn''t belong to the appropriate department!', 16, 1)
 		RETURN
 	END
 
 	UPDATE [dbo].[Reports]
-	   SET
-	       [dbo].[Reports].[EmployeeId] = @EmployeeId
-	 WHERE [dbo].[Reports].[Id] = @ReportId
+	SET
+	    [dbo].[Reports].[EmployeeId] = @EmployeeId
+	WHERE [dbo].[Reports].[Id] = @ReportId
 END
 
 EXEC usp_AssignEmployeeToReport 30, 1
