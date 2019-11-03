49fac16d3cf02afa6da3f334e40ceb8c372c41cb
Simeon Valev <simeonvalev00@gmail.com>
2019-11-03 12:45:41 +0200

    Upload Conditions

---
+++ Entity Framework/02. Exams/02. C# DB Advanced Exam Retake - 18.04.2019/01. Model Defition_Skeleton + Datasets/MusicHub/DataProcessor/Deserializer.cs
@@ -0,0 +1,42 @@
+﻿namespace MusicHub.DataProcessor
+{
+    using System;
+
+    using Data;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data";
+
+        private const string SuccessfullyImportedWriter 
+            = "Imported {0}";
+        private const string SuccessfullyImportedProducerWithPhone 
+            = "Imported {0} with phone: {1} produces {2} albums";
+        private const string SuccessfullyImportedProducerWithNoPhone
+            = "Imported {0} with no phone number produces {1} albums";
+        private const string SuccessfullyImportedSong 
+            = "Imported {0} ({1} genre) with duration {2}";
+        private const string SuccessfullyImportedPerformer
+            = "Imported {0} ({1} songs)";
+
+        public static string ImportWriters(MusicHubDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportProducersAlbums(MusicHubDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportSongs(MusicHubDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportSongPerformers(MusicHubDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entity Framework/02. Exams/02. C# DB Advanced Exam Retake - 18.04.2019/01. Model Defition_Skeleton + Datasets/MusicHub/StartUp.cs
@@ -0,0 +1,104 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.IO;
+
+    using AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+   
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new MusicHubDbContext();
+
+            Mapper.Initialize(config => config.AddProfile<MusicHubProfile>());
+
+            ResetDatabase(context, shouldDropDatabase: true);
+
+            var projectDir = GetProjectDirectory();
+            
+            ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
+            ExportEntities(context, projectDir + @"ExportResults/");
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                transaction.Rollback();
+            }
+        }
+
+        private static void ImportEntities(MusicHubDbContext context, string baseDir, string exportDir)
+        {
+            var writers = DataProcessor.Deserializer.ImportWriters(context,
+                    File.ReadAllText(baseDir + "Actual - ImportWriters.json"));
+            PrintAndExportEntityToFile(writers, exportDir + "ImportWriters.txt");
+
+            var producerAlbums = DataProcessor.Deserializer.ImportProducersAlbums(context,
+                    File.ReadAllText(baseDir + "Actual - ImportProducersAlbums.json"));
+            PrintAndExportEntityToFile(producerAlbums, exportDir + "ImportProducersAlbums.txt");
+
+            var songs = DataProcessor.Deserializer.ImportSongs(context, 
+                File.ReadAllText(baseDir + "Actual - ImportSongs.xml"));
+            PrintAndExportEntityToFile(songs, exportDir + "ImportSongs.txt");
+
+            var performers = DataProcessor.Deserializer.ImportSongPerformers(context, 
+                File.ReadAllText(baseDir + "Actual - ImportSongPerformers.xml"));
+            PrintAndExportEntityToFile(performers, exportDir + "ImportSongPerformers.txt");
+        }
+
+        private static void ExportEntities(MusicHubDbContext context, string exportDir)
+        {
+            var jsonOutput = DataProcessor.Serializer.ExportAlbumsInfo(context, 9);
+            Console.WriteLine(jsonOutput);
+            File.WriteAllText(exportDir + "Actual - AlbumsInfo.json", jsonOutput);
+
+            var xmlOutput = DataProcessor.Serializer.ExportSongsAboveDuration(context, 4);
+            Console.WriteLine(xmlOutput);
+            File.WriteAllText(exportDir + "Actual - SongsAboveDuration.xml", xmlOutput);
+        }
+
+        private static void ResetDatabase(MusicHubDbContext context, bool shouldDropDatabase = false)
+        {
+            if (shouldDropDatabase)
+            {
+                context.Database.EnsureDeleted();
+            }
+
+            if (context.Database.EnsureCreated())
+            {
+                return;
+            }
+
+            var disableIntegrityChecksQuery = "EXEC sp_MSforeachtable @command1='ALTER TABLE ? NOCHECK CONSTRAINT ALL'";
+            context.Database.ExecuteSqlCommand(disableIntegrityChecksQuery);
+
+            var deleteRowsQuery = "EXEC sp_MSforeachtable @command1='SET QUOTED_IDENTIFIER ON;DELETE FROM ?'";
+            context.Database.ExecuteSqlCommand(deleteRowsQuery);
+
+            var enableIntegrityChecksQuery =
+                "EXEC sp_MSforeachtable @command1='ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'";
+            context.Database.ExecuteSqlCommand(enableIntegrityChecksQuery);
+
+            var reseedQuery =
+                "EXEC sp_MSforeachtable @command1='IF OBJECT_ID(''?'') IN (SELECT OBJECT_ID FROM SYS.IDENTITY_COLUMNS) DBCC CHECKIDENT(''?'', RESEED, 0)'";
+            context.Database.ExecuteSqlCommand(reseedQuery);
+        }
+
+        private static void PrintAndExportEntityToFile(string entityOutput, string outputPath)
+        {
+            Console.WriteLine(entityOutput);
+            File.WriteAllText(outputPath, entityOutput.TrimEnd());
+        }
+
+        private static string GetProjectDirectory()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var directoryName = Path.GetFileName(currentDirectory);
+            var relativePath = directoryName.StartsWith("netcoreapp") ? @"../../../" : string.Empty;
+
+            return relativePath;
+        }
+    }
+}
