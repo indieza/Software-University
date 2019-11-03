8c7f0a06d278f46d66c07c5d48a3721d7a6cdb20
Simeon Valev <simeonvalev00@gmail.com>
2019-11-03 15:00:25 +0200

    Upload Conditions

---
+++ Entity Framework/02. Exams/03. C# DB Advanced Exam - 12.08.2018/01. Model Definition_Skeleton and Datasets/SoftJail/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=SoftJail;Trusted_Connection=True";
+    }
+}
