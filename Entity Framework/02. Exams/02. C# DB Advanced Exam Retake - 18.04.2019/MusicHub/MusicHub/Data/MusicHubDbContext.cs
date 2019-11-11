365f639f0e117a322a1297957eb6b6d63dd06081
Simeon Valev <simeonvalev00@gmail.com>
2019-11-11 20:19:46 +0200

    Extract Exam Structure

---
+++ Entity Framework/02. Exams/02. C# DB Advanced Exam Retake - 18.04.2019/MusicHub/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,30 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Entity Framework/02. Exams/02. C# DB Advanced Exam Retake - 18.04.2019/MusicHub/MusicHub/DataProcessor/Serializer.cs
@@ -0,0 +1,19 @@
+﻿namespace MusicHub.DataProcessor
+{
+    using System;
+
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
