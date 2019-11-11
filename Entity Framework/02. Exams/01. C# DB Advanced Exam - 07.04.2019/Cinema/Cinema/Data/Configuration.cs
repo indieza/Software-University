450a44bff73cb6b9b34eb55f907bac2c8223d491
Simeon Valev <simeonvalev00@gmail.com>
2019-11-11 15:46:28 +0200

    Extract Exam Structure

---
+++ Entity Framework/02. Exams/01. C# DB Advanced Exam - 07.04.2019/Cinema/Cinema/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinema.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=Cinema;Trusted_Connection=True";
+    }
+}
