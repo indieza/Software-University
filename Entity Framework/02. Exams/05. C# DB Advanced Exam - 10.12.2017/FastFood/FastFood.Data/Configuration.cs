142e263a3e8d4a0df980a469634a2d60786d2a86
Simeon Valev <simeonvalev00@gmail.com>
2019-11-27 14:45:30 +0200

    Reset Exam

--- Entity Framework/02. Exams/05. C# DB Advanced Exam - 10.12.2017/FastFood/FastFood.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-namespace FastFood.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-40U9D0R\SQLEXPRESS;Database=FastFood;Trusted_Connection=True";
-    }
-}
--- Entity Framework/02. Exams/05. C# DB Advanced Exam - 10.12.2017/FastFood/FastFood.Models/Enums/OrderType.cs
+++
@@ -1,8 +0,0 @@
-namespace FastFood.Models.Enums
-{
-    public enum OrderType
-    {
-        ForHere = 0,
-        ToGo = 1
-    }
-}
