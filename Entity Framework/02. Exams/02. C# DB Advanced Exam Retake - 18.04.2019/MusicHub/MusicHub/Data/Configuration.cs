62f40996ac8771dc371998f73eb146f1c4f9fdd9
Simeon Valev <simeonvalev00@gmail.com>
2019-12-01 17:13:58 +0200

    Reset Exam

--- Entity Framework/02. Exams/02. C# DB Advanced Exam Retake - 18.04.2019/MusicHub/MusicHub/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-40U9D0R\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
-    }
-}
