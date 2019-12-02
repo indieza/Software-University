cfb5e8a003166c79803932bd0bbffa7ccfd321aa
Simeon Valev <simeonvalev00@gmail.com>
2019-12-02 14:53:29 +0200

    Reset Exam

--- Entity Framework/02. Exams/03. C# DB Advanced Exam - 12.08.2018/SoftJail/SoftJail/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SoftJail.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-40U9D0R\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
-    }
-}
