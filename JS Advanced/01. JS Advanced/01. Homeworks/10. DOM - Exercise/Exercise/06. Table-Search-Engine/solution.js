8f4314fa64cfaa440be3653de576de9a562566e8
Simeon Valev <simeonvalev00@gmail.com>
2019-12-24 01:00:00 +0200

    Problem 6 - Done!!!

--- JS Advanced/01. JS Advanced/01. Homeworks/10. DOM - Exercise/Exercise/06. Table-Search-Engine/solution.js
+++ JS Advanced/01. JS Advanced/01. Homeworks/10. DOM - Exercise/Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+    const button = document.getElementById("searchBtn");
+    const rows = document.querySelectorAll("tbody > tr");
+
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        Array.from(rows).map(e => e.classList = "");
+
+        const input = document.getElementById("searchField").value;
+
+        for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+
+            let cells = row.querySelectorAll("td");
+
+            for (const cell of cells) {
+                let cellValue = cell.innerHTML;
+
+                if (cellValue.toLowerCase().includes(input.toLowerCase())) {
+                    row.className = "select";
+                }
+            }
+        }
+
+        document.getElementById("searchField").value = "";
+    });
 }
