9a78b94f17ee428981c085016339171fff5b33b0
Simeon Valev <simeonvalev00@gmail.com>
2019-12-24 14:04:36 +0200

    First Problem - Done!!! - 100/100

--- JS Advanced/01. JS Advanced/02. Exams/01. JS Advanced Exam Preparation - July 2019/01. Form Acceptance/FormAcceptance/scripts/app.js
+++ JS Advanced/01. JS Advanced/02. Exams/01. JS Advanced Exam Preparation - July 2019/01. Form Acceptance/FormAcceptance/scripts/app.js
@@ -1,3 +1,47 @@
 function acceptance() {
-	console.log('TODO...')
+	let divCount = 1;
+
+	const button = document.getElementById("acceptance");
+	const warehouse = document.getElementById("warehouse");
+	button.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		const company = document.getElementsByName("shippingCompany")[0].value;
+		const product = document.getElementsByName("productName")[0].value;
+		const quantity = document.getElementsByName("productQuantity")[0].value;
+		const scrape = document.getElementsByName("productScrape")[0].value;
+
+		if (company !== "" && product !== "" && !isNaN(quantity) && !isNaN(scrape)) {
+			let productQuantity = Number(quantity);
+			let productScrape = Number(scrape);
+
+			if (productQuantity - productScrape > 0) {
+				let outOfStockButton = document.createElement("button");
+				let buttonNode = document.createTextNode("Out of stock");
+				outOfStockButton.appendChild(buttonNode);
+
+				let div = document.createElement("div");
+				div.id = `${divCount++}`;
+
+				let createParagraph = document.createElement("p");
+				let paragraphNode = document
+					.createTextNode(`[${company}] ${product} - ${productQuantity - productScrape} pieces`);
+				createParagraph.appendChild(paragraphNode);
+
+				warehouse.appendChild(div);
+				div.appendChild(createParagraph)
+				div.appendChild(outOfStockButton);
+
+				outOfStockButton.addEventListener("click", function (b) {
+					b.preventDefault();
+					warehouse.removeChild(document.getElementById(div.id));
+				})
+			}
+		}
+
+		document.getElementsByName("shippingCompany")[0].value = "";
+		document.getElementsByName("productName")[0].value = "";
+		document.getElementsByName("productQuantity")[0].value = "";
+		document.getElementsByName("productScrape")[0].value = "";
+	})
 }
