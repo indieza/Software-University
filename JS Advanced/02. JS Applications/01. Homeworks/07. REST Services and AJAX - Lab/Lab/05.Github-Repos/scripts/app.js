bee7b77796c3132b6a9d74f3b693ed0c90b891ed
Simeon Valev <simeonvalev00@gmail.com>
2020-01-10 00:28:06 +0200

    Last Lab Problem - Done!!!

--- JS Advanced/02. JS Applications/01. Homeworks/07. REST Services and AJAX - Lab/Lab/05.Github-Repos/scripts/app.js
+++ JS Advanced/02. JS Applications/01. Homeworks/07. REST Services and AJAX - Lab/Lab/05.Github-Repos/scripts/app.js
@@ -1,3 +1,21 @@
 function loadRepos() {
-	console.log("TODO...");
+	const repositories = document.getElementById("repos");
+	repositories.innerHTML = "";
+	const username = document.getElementById("username").value;
+	const link = `https://api.github.com/users/${username}/repos`;
+
+	fetch(link)
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach(item => {
+				const li = document.createElement("li");
+				const link = document.createElement("a");
+				link.href = item.html_url;
+				link.textContent = item.full_name;
+
+				li.appendChild(link);
+				repositories.appendChild(li);
+			});
+		})
+		.catch();
 }
