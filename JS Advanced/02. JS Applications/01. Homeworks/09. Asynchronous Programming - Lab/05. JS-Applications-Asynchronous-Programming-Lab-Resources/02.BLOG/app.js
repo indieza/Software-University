f751ed32cfcb303e3b540992bfbf0d1f49d4e915
Simeon Valev <simeonvalev00@gmail.com>
2020-01-13 12:08:51 +0200

    Problem 2 - Halfly DONE!!! - Bug with FireBase DB

--- JS Advanced/02. JS Applications/01. Homeworks/09. Asynchronous Programming - Lab/05. JS-Applications-Asynchronous-Programming-Lab-Resources/02.BLOG/app.js
+++ JS Advanced/02. JS Applications/01. Homeworks/09. Asynchronous Programming - Lab/05. JS-Applications-Asynchronous-Programming-Lab-Resources/02.BLOG/app.js
@@ -1,5 +1,57 @@
+const baseUrl = `https://blog-apps-c12bf.firebaseio.com/posts`;
+const postsData = document.getElementById("posts");
+
 function attachEvents() {
-    console.log('TODO...');
+    function loadPosts() {
+        fetch(`${baseUrl}.json`)
+            .then(parseData())
+            .then(data => {
+                Object.entries(data)
+                    .forEach(([value, element]) => {
+                        const option = document.createElement("option");
+                        option.value = value;
+                        option.id = element.id;
+                        option.innerHTML = element.title;
+                        postsData.appendChild(option);
+                    });
+            })
+            .catch(printError())
+    }
+
+
+    function viewPost() {
+        const postId = postsData.value;
+
+        fetch(`${baseUrl}/${postId}`)
+            .then(parseData())
+            .then(data => {
+
+            })
+            .catch(printError());
+    }
+
+    function parseData() {
+        return resources => {
+            if (!resources.ok) {
+                throw new Error(JSON.stringify({
+                    status: resources.status,
+                    statusText: resources.statusText
+                }));
+            }
+            return resources.json();
+        };
+    }
+
+    function printError() {
+        return (errorData) => {
+            console.log(errorData.message);
+        };
+    }
+
+    return {
+        loadPosts,
+        viewPost
+    };
 }
 
-attachEvents();
+const result = attachEvents();
