acdaed436b6d2251f6093f6d626bb02980aa1075
Simeon Valev <simeonvalev00@gmail.com>
2020-01-13 11:32:27 +0200

    Problem 1 - Done!!!

--- JS Advanced/02. JS Applications/01. Homeworks/09. Asynchronous Programming - Lab/05. JS-Applications-Asynchronous-Programming-Lab-Resources/01.GITHUB-COMMITS/app.js
+++ JS Advanced/02. JS Applications/01. Homeworks/09. Asynchronous Programming - Lab/05. JS-Applications-Asynchronous-Programming-Lab-Resources/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,39 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById("username").value;
+    const repository = document.getElementById("repo").value;
+    const commits = document.getElementById("commits");
+    commits.innerHTML = "";
+
+    const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+    try {
+        await fetch(url)
+            .then(resources => {
+                if (!resources.ok) {
+                    throw new Error(JSON.stringify({
+                        status: resources.status,
+                        statusText: resources.statusText
+                    }));
+                }
+
+                return resources.json();
+            })
+            .then(data => {
+                Object.entries(data)
+                    .forEach(([_, data]) => {
+
+                        const authorName = data.commit.author.name;
+                        const message = data.commit.message;
+
+                        const li = document.createElement("li");
+                        li.textContent = `${authorName}: ${message}`;
+                        commits.appendChild(li);
+                    });
+            })
+    } catch (errorData) {
+        const error = JSON.parse(errorData.message);
+        const li = document.createElement("li");
+        li.textContent = `Error: ${error.status} (${error.statusText})`;
+        commits.appendChild(li);
+    }
 }
