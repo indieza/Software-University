673ab274dd244f9ee00d2e31aad7cb3478662e13
Simeon Valev <simeonvalev00@gmail.com>
2020-01-28 00:23:36 +0200

    Upload Catalog Page

--- JS Advanced/02. JS Applications/01. Homeworks/16. Routing and Architecture - Exercise/Team-Manager/scripts/app.js
+++ JS Advanced/02. JS Applications/01. Homeworks/16. Routing and Architecture - Exercise/Team-Manager/scripts/app.js
@@ -1,110 +1,130 @@
- import {
-     get,
-     post,
-     put,
-     del
- } from "./requester.js";
+// To create Catalog Collection go to Kinvey, use that URL:
+// Use POST API!!!
+//https://baas.kinvey.com/appdata/{app_id}/{collection_name}
+// Take token from Chrome Console - sessionStorage.getItem("authtoken")
 
- (() => {
-     const partials = {
-         header: "./templates/common/header.hbs",
-         footer: "./templates/common/footer.hbs",
-         loginForm: "./templates/login/loginForm.hbs",
-         registerForm: "./templates/register/registerForm.hbs"
-     };
+// Choose Headers -> then fill:
+// KEY - Authorization, VALUE-Kinvey authtoken
+// KEY - Content-Type, VALUE-application/json
 
-     const app = new Sammy("#main", function () {
-         this.use("Handlebars", "hbs");
+// Go to body section:
+// Choose raw data - JSON TEXT
+// Fill the data!!!
 
-         this.get("index.html", function (context) {
-             loadPage(context, "./templates/home/home.hbs");
-         });
+import {
+    get,
+    post,
+    put,
+    del
+} from "./requester.js";
 
-         this.get("#/", function (context) {
-             loadPage(context, "./templates/home/home.hbs");
-         });
+(() => {
+    const partials = {
+        header: "./templates/common/header.hbs",
+        footer: "./templates/common/footer.hbs",
+        loginForm: "./templates/login/loginForm.hbs",
+        registerForm: "./templates/register/registerForm.hbs",
+        team: "./templates/catalog/team.hbs"
+    };
 
-         this.get("#/home", function (context) {
-             loadPage(context, "./templates/home/home.hbs");
-         });
+    const app = new Sammy("#main", function () {
+        this.use("Handlebars", "hbs");
 
-         this.get("#/about", function (context) {
-             loadPage(context, "./templates/about/about.hbs");
-         });
+        this.get("index.html", function (context) {
+            loadPage(context, "./templates/home/home.hbs");
+        });
 
-         this.get("#/create", function (context) {
-             loadPage(context, "./templates/create/createPage.hbs");
-         });
+        this.get("#/", function (context) {
+            loadPage(context, "./templates/home/home.hbs");
+        });
 
-         this.get("#/login", function (context) {
-             loadPage(context, "./templates/login/loginPage.hbs");
-         });
+        this.get("#/home", function (context) {
+            loadPage(context, "./templates/home/home.hbs");
+        });
 
-         this.post("#/login", function (context) {
-             const {
-                 username,
-                 password
-             } = context.params;
-             post("user", "login", {
-                     username,
-                     password
-                 }, "Basic")
-                 .then(resources => saveUserInfo(resources))
-                 .then(() => context.redirect("#/home"))
-                 .catch(console.error);
-         })
+        this.get("#/about", function (context) {
+            loadPage(context, "./templates/about/about.hbs");
+        });
 
-         this.get("#/logout", function (context) {
-             sessionStorage.clear();
-             context.redirect("#/home");
-         });
+        this.get("#/create", function (context) {
+            loadPage(context, "./templates/create/createPage.hbs");
+        });
 
-         this.get("#/register", function (context) {
-             loadPage(context, "./templates/register/registerPage.hbs");
-         });
+        this.get("#/login", function (context) {
+            loadPage(context, "./templates/login/loginPage.hbs");
+        });
 
-         this.post("#/register", function (context) {
-             const {
-                 username,
-                 password,
-                 repeatPassword
-             } = context.params;
-             if (username.length >= 3 && password.length >= 3 && password === repeatPassword) {
-                 post("user", "", {
-                         username,
-                         password
-                     }, "Basic")
-                     .then(resources => saveUserInfo(resources))
-                     .then(() => context.redirect("#/home"))
-                     .catch(console.error);
-             }
-         });
+        this.post("#/login", function (context) {
+            const {
+                username,
+                password
+            } = context.params;
+            post("user", "login", {
+                    username,
+                    password
+                }, "Basic")
+                .then(resources => saveUserInfo(resources))
+                .then(() => context.redirect("#/home"))
+                .catch(console.error);
+        })
 
-         this.get("#/catalog", function (context) {
+        this.get("#/logout", function (context) {
+            sessionStorage.clear();
+            context.redirect("#/home");
+        });
 
-         });
-     });
+        this.get("#/register", function (context) {
+            loadPage(context, "./templates/register/registerPage.hbs");
+        });
 
-     app.run();
+        this.post("#/register", function (context) {
+            const {
+                username,
+                password,
+                repeatPassword
+            } = context.params;
+            if (username.length >= 3 && password.length >= 3 && password === repeatPassword) {
+                post("user", "", {
+                        username,
+                        password
+                    }, "Basic")
+                    .then(resources => saveUserInfo(resources))
+                    .then(() => context.redirect("#/home"))
+                    .catch(console.error);
+            }
+        });
 
-     function getUserInfo(context) {
-         context.loggedIn = sessionStorage.getItem("authtoken");
-         context.username = sessionStorage.getItem("username");
-     }
+        this.get("#/catalog", function (context) {
+            get("appdata", "teams", "Kinvey")
+                .then(data => {
+                    context.teams = data;
+                    loadPage(context, "./templates/catalog/teamCatalog.hbs");
+                    console.log(data);
+                })
+                .catch(console.error);
+        });
+    });
 
-     function loadPage(context, path) {
-         getUserInfo(context);
+    app.run();
 
-         context.loadPartials(partials)
-             .then(function () {
-                 this.partial(path);
-             });
-     }
+    function getUserInfo(context) {
+        context.loggedIn = sessionStorage.getItem("authtoken");
+        context.username = sessionStorage.getItem("username");
+    }
 
-     function saveUserInfo(resources) {
-         if (resources.username && resources._kmd.authtoken) {
-             sessionStorage.setItem('username', resources['username']);
-             sessionStorage.setItem('authtoken', resources._kmd['authtoken']);
-         }
-     }
- })();
+    function loadPage(context, path) {
+        getUserInfo(context);
+
+        context.loadPartials(partials)
+            .then(function () {
+                this.partial(path);
+            });
+    }
+
+    function saveUserInfo(resources) {
+        if (resources.username && resources._kmd.authtoken) {
+            sessionStorage.setItem('username', resources['username']);
+            sessionStorage.setItem('authtoken', resources._kmd['authtoken']);
+        }
+    }
+})();
