e4052145ee40fee2191fb4c4cc6ac9743f0d75f9
Simeon Valev <simeonvalev00@gmail.com>
2020-01-27 18:48:06 +0200

    Upload Base Structure

--- JS Advanced/02. JS Applications/01. Homeworks/16. Routing and Architecture - Exercise/Team-Manager-Skeleton/scripts/app.js
+++ JS Advanced/02. JS Applications/01. Homeworks/16. Routing and Architecture - Exercise/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,43 @@
- // TODO
+ import {
+     get,
+     post,
+     put,
+     del
+ } from "./requester.js";
+
+ (() => {
+     const partials = {
+         header: "./templates/common/header.hbs",
+         footer: "./templates/common/footer.hbs"
+     };
+
+     const app = new Sammy("#main", function () {
+         this.use("Handlebars", "hbs");
+
+         this.get("index.html", function (context) {
+             loadPage(context, "./templates/home/home.hbs");
+         });
+
+         this.get("#/", function (context) {
+             loadPage(context, "./templates/home/home.hbs");
+         });
+
+         this.get("#/home", function (context) {
+             loadPage(context, "./templates/home/home.hbs");
+         });
+     });
+
+     function getUserInfo(context) {
+         context.loggedIn = sessionStorage.getItem("authtoken");
+         context.username = sessionStorage.getItem("username");
+     }
+
+     function loadPage(context, path) {
+         getUserInfo(context);
+
+         context.loadPartials(partials)
+             .then(function () {
+                 this.partial(path);
+             });
+     }
+ })();
---
+++ JS Advanced/02. JS Applications/01. Homeworks/16. Routing and Architecture - Exercise/Team-Manager-Skeleton/scripts/requester.js
@@ -0,0 +1,64 @@
+const baseUrl = "https://baas.kinvey.com";
+const appKey = "kid_B1jEweJb8";
+const appSecret = "e87aa05ec46d4799b45f4f1ce54341f8";
+
+function createAuthorization(type){
+    return type === 'Basic'
+    ? `Basic ${btoa(`${appKey}:${appSecret}`)}`
+    : `Kinvey ${sessionStorage.getItem('authtoken')}`
+}
+
+function makeHeaders(type, method, data) {
+    const headers = {
+        method: method,
+        headers: {
+            'Authorization': createAuthorization(type),
+            'Content-Type': 'application/json'
+        }
+    };
+
+    if (method === "POST" || method === "PUT") {
+        headers.body = JSON.stringify(data);
+    }
+
+    return headers;
+}
+
+function handleError(e) {
+    if (!e.ok) {
+        throw new Error(`${e.status} - ${e.statusText}`);
+    }
+
+    return e;
+}
+
+function serializeData(x) {
+    return x.json();
+}
+
+function fetchRequest(kinveyModule, endpoint, headers) {
+    const url = `${baseUrl}/${kinveyModule}/${appKey}/${endpoint}`;
+    return fetch(url, headers)
+        .then(handleError)
+        .then(serializeData);
+}
+
+export function get(kinveyModule, endpoint, type) {
+    const headers = makeHeaders(type, "GET");
+    return fetchRequest(kinveyModule, endpoint, headers);
+}
+
+export function post(kinveyModule, endpoint, data, type) {
+    const headers = makeHeaders(type, "POST", data);
+    return fetchRequest(kinveyModule, endpoint, headers);
+}
+
+export function put(kinveyModule, endpoint, data, type) {
+    const headers = makeHeaders(type, "PUT", data);
+    return fetchRequest(kinveyModule, endpoint, headers);
+}
+
+export function del(kinveyModule, endpoint, type) {
+    const headers = makeHeaders(type, "DELETE");
+    return fetchRequest(kinveyModule, endpoint, headers);
+}
