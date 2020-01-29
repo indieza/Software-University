30cd6185da5b73361baadd1a4cba127ae9ea2b74
Simeon Valev <simeonvalev00@gmail.com>
2020-01-29 14:19:52 +0200

    Make likes, list recipes, edit and make recipe

--- JS Advanced/02. JS Applications/02. Exams/01. JS Applications Retake - 20 April 2019/CookUni/scripts/app.js
+++ JS Advanced/02. JS Applications/02. Exams/01. JS Applications Retake - 20 April 2019/CookUni/scripts/app.js
@@ -6,17 +6,38 @@ import {
 } from "../scripts/requester.js";
 
 (() => {
+    const categories = {
+        "Grain Food": "https://cdn.pixabay.com/photo/2014/12/11/02/55/corn-syrup-563796__340.jpg",
+        "Fruits": "https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029__340.jpg",
+        "Milk, cheese, eggs and alternatives": "https://image.shutterstock.com/image-photo/assorted-dairy-products-milk-yogurt-260nw-530162824.jpg",
+        "Lean meats and poultry, fish and alternatives": "https://t3.ftcdn.net/jpg/01/18/84/52/240_F_118845283_n9uWnb81tg8cG7Rf9y3McWT1DT1ZKTDx.jpg",
+        "Vegetables and legumes/beans": "https://t3.ftcdn.net/jpg/00/25/90/48/240_F_25904887_fhZJ692ukng3vQxzHldvuN981OiYVlJ1.jpg"
+    };
+
     const app = new Sammy("#rooter", function () {
         this.use("Handlebars", "hbs");
 
-        this.get("/index.html", function (ctx) {
+        this.get("/index.html", async function (ctx) {
             setHeaderInfo(ctx);
 
-            this.loadPartials(getPartials())
-                .partial("../templates/home.hbs");
+            if (ctx.isLogged) {
+                await get("appdata", "recipes", "Kinvey")
+                    .then(recipes => {
+                        ctx.recipes = recipes;
+                    })
+                    .catch(console.error);
+
+                this.loadPartials(getPartials())
+                    .partial("../templates/home.hbs");
+            } else {
+                this.loadPartials(getPartials())
+                    .partial("../templates/home.hbs");
+            }
         });
 
         this.get("/register", function (ctx) {
+            setHeaderInfo(ctx);
+
             this.loadPartials(getPartials())
                 .partial("../templates/admin/register.hbs");
         });
@@ -46,6 +67,8 @@ import {
         });
 
         this.get("/login", function (ctx) {
+            setHeaderInfo(ctx);
+
             this.loadPartials(getPartials())
                 .partial("../templates/admin/login.hbs");
         });
@@ -77,6 +100,161 @@ import {
                 })
                 .catch(console.error);
         });
+
+        this.get("/share", function (ctx) {
+            setHeaderInfo(ctx);
+
+            this.loadPartials(getPartials())
+                .partial("../templates/recipe/share.hbs");
+        });
+
+        this.post("/share", function (ctx) {
+            const {
+                meal,
+                ingredients,
+                prepMethod,
+                description,
+                foodImageURL,
+                category
+            } = ctx.params;
+
+            if (meal && ingredients && prepMethod && description && foodImageURL && category) {
+                post("appdata", "recipes", {
+                        meal,
+                        ingredients: ingredients.split(" "),
+                        prepMethod,
+                        description,
+                        foodImageURL,
+                        category,
+                        likesCounter: 0,
+                        categoryImageURL: categories[category]
+                    })
+                    .then(() => {
+                        ctx.redirect("/index.html");
+                    })
+                    .catch(console.error);
+            }
+        });
+
+        this.get("/recipe/:id", function (ctx) {
+            const id = ctx.params.id;
+            setHeaderInfo(ctx);
+
+            get("appdata", `recipes/${id}`, "Kinvey")
+                .then(recipe => {
+                    recipe.isCreator = sessionStorage.getItem("userId") === recipe._acl.creator;
+
+                    ctx.recipe = recipe;
+                    this.loadPartials(getPartials())
+                        .partial("../templates/recipe/recipeDetails.hbs");
+                })
+                .catch(console.error);
+        });
+
+        this.get("/edit/:id", function (ctx) {
+            const id = ctx.params.id;
+            setHeaderInfo(ctx);
+
+            get("appdata", `recipes/${id}`, "Kinvey")
+                .then(recipe => {
+                    recipe.ingredients = recipe.ingredients.join(" ");
+                    ctx.recipe = recipe;
+
+                    this.loadPartials(getPartials())
+                        .partial("../templates/recipe/recipeEdit.hbs");
+                })
+                .catch(console.error);
+        });
+
+        this.post("/edit/:id", function (ctx) {
+            const id = ctx.params.id;
+            const {
+                meal,
+                ingredients,
+                prepMethod,
+                description,
+                foodImageURL,
+                category,
+                likesCounter
+            } = ctx.params;
+
+            put("appdata", `recipes/${id}`, {
+                    meal,
+                    ingredients: ingredients.split(" "),
+                    prepMethod,
+                    description,
+                    foodImageURL,
+                    category,
+                    likesCounter,
+                    categoryImageURL: categories[category]
+                })
+                .then(() => {
+                    ctx.redirect("/index.html");
+                })
+                .catch(console.error);
+        });
+
+        this.get("/archive/:id", function (ctx) {
+            setHeaderInfo(ctx);
+            const id = ctx.params.id;
+
+            del("appdata", `recipes/${id}`, "Kinvey")
+                .then(() => {
+                    return ctx.redirect("/index.html");
+                })
+                .catch(console.error);
+        });
+
+        this.get("/like/:id", async function (ctx) {
+            const id = ctx.params.id;
+            setHeaderInfo(ctx);
+
+            const recipe = await get("appdata", `recipes/${id}`, "Kinvey");
+
+            put("appdata", `recipes/${id}`, {
+                    meal: recipe.meal,
+                    ingredients: recipe.ingredients,
+                    prepMethod: recipe.prepMethod,
+                    description: recipe.description,
+                    foodImageURL: recipe.foodImageURL,
+                    category: recipe.category,
+                    likesCounter: recipe.likesCounter + 1,
+                    categoryImageURL: recipe.categoryImageURL
+                })
+                .then(() => {
+                    ctx.redirect("/index.html");
+                })
+                .catch(console.error);
+        });
+
+        this.post("/like/:id", function (ctx) {
+            const id = ctx.params.id;
+            setHeaderInfo(ctx);
+            const {
+                meal,
+                ingredients,
+                prepMethod,
+                description,
+                foodImageURL,
+                category,
+                likesCounter
+            } = ctx.params;
+
+            put("appdata", `recipes/${id}`, {
+                    meal,
+                    ingredients: ingredients.split(" "),
+                    prepMethod,
+                    description,
+                    foodImageURL,
+                    category,
+                    likesCounter: likesCounter + 1,
+                    categoryImageURL: categories[category]
+                })
+                .then(() => {
+                    ctx.redirect("/index.html");
+                })
+                .catch(console.error);
+        });
     });
 
     app.run();
@@ -84,7 +262,9 @@ import {
     function getPartials() {
         return {
             header: "../templates/common/header.hbs",
-            footer: "../templates/common/footer.hbs"
+            footer: "../templates/common/footer.hbs",
+            homeAnonymous: "../templates/homeAnonymous.hbs",
+            homeUser: "../templates/homeUser.hbs"
         }
     }
 
@@ -96,5 +276,6 @@ import {
     function saveAuthInfo(userInfo) {
         sessionStorage.setItem("authtoken", userInfo._kmd.authtoken);
         sessionStorage.setItem("names", `${userInfo.firstName} ${userInfo.lastName}`);
+        sessionStorage.setItem("userId", userInfo._id);
     }
 })();
