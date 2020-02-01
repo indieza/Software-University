2e710dc9ed015d9737ef84a6e6a80ab49d25d7bd
Simeon Valev <simeonvalev00@gmail.com>
2020-02-01 11:02:22 +0200

    Make base logic, left bonus tasks, validate fields and update hyperlinks

--- JS Advanced/02. JS Applications/02. Exams/03. JS Applications - Retake Exam 13 December/SoftTerest/scripts/app.js
+++ JS Advanced/02. JS Applications/02. Exams/03. JS Applications - Retake Exam 13 December/SoftTerest/scripts/app.js
@@ -78,6 +78,132 @@
                   .catch(console.error);
           });
 
+          this.get("/create", function (ctx) {
+              setHeaderInfo(ctx);
+
+              this.loadPartials(getPartials())
+                  .partial("../templates/idea/create.hbs");
+          });
+
+          this.post("/create", function (ctx) {
+              const {
+                  title,
+                  description,
+                  imageURL
+              } = ctx.params;
+
+              if (title.length >= 6 && description.length >= 10 &&
+                  (imageURL.startsWith("http://") || imageURL.startsWith("https://"))) {
+                  post("appdata", "ideas", {
+                          title,
+                          description,
+                          imageURL,
+                          creator: sessionStorage.getItem("username"),
+                          likes: 0,
+                          comments: []
+                      })
+                      .then(() => {
+                          ctx.redirect("/index.html");
+                      })
+                      .catch(console.error);
+              }
+          });
+
+          this.get("/delete/:id", function (ctx) {
+              setHeaderInfo(ctx);
+              const id = ctx.params.id;
+
+              del("appdata", `ideas/${id}`, "Kinvey")
+                  .then(() => {
+                      return ctx.redirect("/index.html");
+                  })
+                  .catch(console.error);
+          });
+
+          this.get("/dashboard", async function (ctx) {
+              setHeaderInfo(ctx);
+
+              await get("appdata", "ideas", "Kinvey")
+                  .then(ideas => {
+                      ctx.ideas = ideas;
+                  })
+                  .catch(console.error);
+
+              this.loadPartials(getPartials())
+                  .partial("../templates/idea/dashboard.hbs");
+          });
+
+          this.get("/idea/:id", function (ctx) {
+              const id = ctx.params.id;
+              setHeaderInfo(ctx);
+
+              get("appdata", `ideas/${id}`, "Kinvey")
+                  .then(idea => {
+                      idea.isCreator = sessionStorage.getItem("userId") === idea._acl.creator;
+
+                      ctx.idea = idea;
+                      this.loadPartials(getPartials())
+                          .partial("../templates/idea/details.hbs");
+                  })
+                  .catch(console.error);
+          });
+
+          this.get("/like/:id", async function (ctx) {
+              const id = ctx.params.id;
+              setHeaderInfo(ctx);
+
+              const idea = await get("appdata", `ideas/${id}`, "Kinvey");
+
+              put("appdata", `ideas/${id}`, {
+                      title: idea.title,
+                      description: idea.description,
+                      imageURL: idea.imageURL,
+                      creator: idea.creator,
+                      likes: idea.likes + 1,
+                      comments: idea.comments
+                  })
+                  .then(() => {
+                      ctx.redirect(`/index.html`);
+                  })
+                  .catch(console.error);
+          });
+
+          this.get("/comment/:id", async function (ctx) {
+              const id = ctx.params.id;
+              const newComment = ctx.params.newComment;
+              setHeaderInfo(ctx);
+
+              const idea = await get("appdata", `ideas/${id}`, "Kinvey");
+              const comments = idea.comments;
+              comments.push(`${sessionStorage.getItem("username")}: ${newComment}`);
+
+              put("appdata", `ideas/${id}`, {
+                      title: idea.title,
+                      description: idea.description,
+                      imageURL: idea.imageURL,
+                      creator: idea.creator,
+                      likes: idea.likes,
+                      comments: comments
+                  })
+                  .then(() => {
+                      ctx.redirect(`/index.html`);
+                  })
+                  .catch(console.error);
+          });
+
+          this.get("/profile", async function (ctx) {
+              setHeaderInfo(ctx);
+              const allIdeas = await get("appdata", "ideas", "Kinvey");
+
+              const ideas = allIdeas.filter(t => t.creator === sessionStorage.getItem("username"));
+              ctx.username = sessionStorage.getItem("username");
+              ctx.ideasCount = ideas.length;
+              ctx.ideas = ideas.map(x => x.title);
+
+
+              this.loadPartials(getPartials())
+                  .partial("../templates/admin/profile.hbs");
+          })
       });
 
       app.run();
@@ -85,8 +211,7 @@
       function getPartials() {
           return {
               header: "../templates/common/header.hbs",
-              footer: "../templates/common/footer.hbs",
-              guestHome: "../templates/guestHome.hbs"
+              footer: "../templates/common/footer.hbs"
           }
       }
 
