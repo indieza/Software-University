316bfd95dacbf08b6cb534cff3a61f46ac36a59a
Simeon Valev <simeonvalev00@gmail.com>
2020-02-03 15:46:19 +0200

    Make 80/100 on Problem 1

--- JS Advanced/01. JS Advanced/02. Exams/04. JS Advanced - 26 October 2019/01. Ski Inventory/Ski Inventory/solution.js
+++ JS Advanced/01. JS Advanced/02. Exams/04. JS Advanced - 26 October 2019/01. Ski Inventory/Ski Inventory/solution.js
@@ -1,3 +1,117 @@
 function solve() {
-   //ToDO...
+    let totalPrice = 0;
+
+    const html = {
+        availableProductsList: () => document.querySelector("#products > ul"),
+        myProductsList: () => document.querySelector("#myProducts > ul"),
+        addButton: () => document.getElementsByTagName("button")[1],
+        filterButton: () => document.querySelector("#products > div > button"),
+        buyButton: () => document.querySelector("#myProducts > button"),
+        productFilterInput: () => document.getElementsByTagName("input")[0],
+        productNameInput: () => document.querySelectorAll('#add-new input')[0],
+        productQuantityInput: () => document.querySelectorAll('#add-new input')[1],
+        productPriceInput: () => document.querySelectorAll('#add-new input')[2],
+        totalPriceField: () => document.getElementsByTagName("h1")[1]
+    };
+
+    html.addButton().addEventListener("click", addProductToList);
+    html.buyButton().addEventListener("click", buyProducts);
+    html.filterButton().addEventListener("click", filterProducts);
+
+    function addProductToList(e) {
+        e.preventDefault();
+
+        const name = html.productNameInput().value;
+        const quantity = Number(html.productQuantityInput().value);
+        const price = Number(html.productPriceInput().value);
+
+        const liElement = createDomElement("li", null, null, null);
+        const nameSpan = createDomElement("span", name, null, null);
+        const quantityStrong = createDomElement("strong", `Available: ${quantity}`, null, null);
+        const infoDiv = createDomElement("div", null, null, null);
+        const priceStrong = createDomElement("strong", price.toFixed(2), null, null);
+        const addButton = createDomElement("button", "Add to Client's List");
+        addButton.addEventListener("click", addProductToBuy);
+
+        infoDiv.appendChild(priceStrong);
+        infoDiv.appendChild(addButton);
+        liElement.appendChild(nameSpan);
+        liElement.appendChild(quantityStrong);
+        liElement.appendChild(infoDiv);
+        html.availableProductsList().appendChild(liElement);
+
+        html.productNameInput().value = "";
+        html.productQuantityInput().value = "";
+        html.productPriceInput().value = "";
+    }
+
+    function addProductToBuy(e) {
+        e.preventDefault();
+        const availableQuantity = parseInt(this.parentNode.parentNode.children[1].innerHTML.split(" ")[1]);
+
+        if (availableQuantity === 1) {
+            this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+            createBuyProduct(this);
+        } else {
+            this.parentNode.parentNode.children[1].innerHTML = `Available: ${availableQuantity - 1}`;
+            createBuyProduct(this);
+        }
+    }
+
+    function createBuyProduct(button) {
+        const infoLi = createDomElement("li", button.parentNode.parentNode.children[0].innerHTML, null, null);
+        const priceStrong = createDomElement("strong", Number(button.parentNode.children[0].innerHTML).toFixed(2), null, null);
+        totalPrice += Number(priceStrong.innerHTML);
+        html.totalPriceField().innerHTML = `Total Price: ${totalPrice.toFixed(2)}`;
+        infoLi.appendChild(priceStrong);
+        html.myProductsList().appendChild(infoLi);
+    }
+
+    function buyProducts(e) {
+        e.preventDefault();
+
+        totalPrice = 0;
+        html.totalPriceField().innerHTML = `Total Price: ${totalPrice.toFixed(2)}`;
+        html.myProductsList().innerHTML = "";
+    }
+
+    function filterProducts(e) {
+        e.preventDefault();
+        const filterValue = html.productFilterInput().value;
+        const allAvailableProducts = document.querySelectorAll("#products ul li span");
+
+        allAvailableProducts.forEach(product => {
+            if (!product.innerHTML.toLocaleLowerCase().includes(filterValue)) {
+                product.parentNode.style.display = "none";
+            } else {
+                product.parentNode.style.display = "block";
+            }
+        });
+
+        html.productFilterInput().value = "";
+    }
+
+    /**
+     * @param {string} type Type of the DOM element
+     * @param {string} text Text of the DOM element
+     * @param {Array} classes Array of all DOM classes
+     * @param {Number} id Od of the DOM element
+     */
+    function createDomElement(type, text, classes, id) {
+        let element = document.createElement(type);
+
+        if (text) {
+            element.innerHTML = text;
+        }
+
+        if (classes) {
+            element.classList.add(...classes);
+        }
+
+        if (id) {
+            element.id = id;
+        }
+
+        return element;
+    }
 }
